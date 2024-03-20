417b1e4ea74917ef207d3e72d42497c0f55f9001
Daniele Ieva <daniele.ieva@hotmail.com>
2024-03-20 18:08:25 +0100

    feat: completed it.polimi.ingsw.model.objectives.Objective prototype

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/objectives/Objective.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/objectives/Objective.java
@@ -1,4 +1,17 @@
 package it.polimi.ingsw.model.objectives;
 
+import it.polimi.ingsw.model.Game;
+
+import java.util.function.Function;
+
 public class Objective {
+    private final Function<Game, Integer> pointCalculator;
+
+    public Objective(Function<Game, Integer> pointCalculator) {
+        this.pointCalculator = pointCalculator;
+    }
+
+    public Integer getPoints(Game game) {
+        return this.pointCalculator.apply(game);
+    }
 }
