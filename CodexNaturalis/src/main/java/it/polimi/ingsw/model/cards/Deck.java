dba4604503a330cd5f093fc38d11945835437c16
Daniele Ieva <daniele.ieva@hotmail.com>
2024-03-20 18:12:55 +0100

    feat: completed it.polimi.ingsw.model.cards.Deck prototype

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Deck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Deck.java
@@ -1,4 +1,28 @@
 package it.polimi.ingsw.model.cards;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
+
+    private final ArrayList<Card> cards;
+    public Deck(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
+
+    public void shuffle() {
+        synchronized (cards) {
+            Collections.shuffle(cards);
+        }
+    }
+
+    public Card draw() {
+        synchronized (cards) {
+            return cards.removeFirst();
+        }
+    }
+
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
 }
