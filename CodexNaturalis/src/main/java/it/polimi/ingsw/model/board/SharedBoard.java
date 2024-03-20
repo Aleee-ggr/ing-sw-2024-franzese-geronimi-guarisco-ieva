b217ba9f10e128944de1dea4a5095553e62cf6e2
Daniele Ieva <daniele.ieva@hotmail.com>
2024-03-20 18:47:46 +0100

    feat: created it.polimi.ingsw.model.board.SharedBoard prototype

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/board/SharedBoard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/board/SharedBoard.java
@@ -1,4 +1,30 @@
 package it.polimi.ingsw.model.board;
 
+import it.polimi.ingsw.model.objectives.Objective;
+import it.polimi.ingsw.model.player.Player;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class SharedBoard {
+    public SharedBoard(DeckArea deckArea) {
+        this.deckArea = deckArea;
+    }
+
+    public Map<Player, Integer> getScore() {
+        return new ConcurrentHashMap<>(scoreMap); // Copy the map instead of returning the object itself
+    }
+
+    public Objective[] getGlobalObjectives() {
+        return Arrays.copyOf(objectives, 2);
+    }
+
+    public DeckArea getDeckArea() {
+        return this.deckArea;
+    }
+
+    private final Objective[] objectives = new Objective[2];
+    private final ConcurrentHashMap<Player, Integer> scoreMap = new ConcurrentHashMap<>();
+    private final DeckArea deckArea;
 }
