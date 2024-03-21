58609a77a679c84091d13dec412f4cf3d0dd04d3
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-21 14:07:37 +0100

    feat: created enums

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/enums/GameState.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/enums/GameState.java
@@ -1,4 +1,5 @@
 package it.polimi.ingsw.model.enums;
 
 public enum GameState {
+    LOGIN, START, IN_GAME, END
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/enums/Resource.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/enums/Resource.java
@@ -1,4 +1,5 @@
 package it.polimi.ingsw.model.enums;
 
 public enum Resource {
+    MUSHROOM, LEAF, WOLF, BUTTERFLY, INK, QUILL, SCROLL, NONE
 }
