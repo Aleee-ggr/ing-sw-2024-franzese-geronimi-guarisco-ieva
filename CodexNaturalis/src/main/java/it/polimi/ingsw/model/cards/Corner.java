a817095c82874906dbd6a0c383a67df296541748
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-21 16:17:23 +0100

    feat: created Corner prototype

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Corner.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Corner.java
@@ -1,4 +1,22 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.GameConsts;
+import it.polimi.ingsw.model.enums.Resource;
+
 public class Corner {
+    private final Resource cornerResource;
+    private final boolean coverable;
+
+    public Corner(Resource cornerResource, boolean coverable){
+        this.cornerResource = cornerResource;
+        this.coverable = coverable;
+    }
+
+    public Resource getCornerResource() {
+        return cornerResource;
+    }
+
+    public boolean isCoverable() {
+        return coverable;
+    }
 }
