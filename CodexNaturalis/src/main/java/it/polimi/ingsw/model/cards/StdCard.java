89926b1dba03627b2a669f1779c19d91ce724cea
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-03-21 17:06:14 +0100

    feat: created StdCard prototype

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StdCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StdCard.java
@@ -1,4 +1,15 @@
 package it.polimi.ingsw.model.cards;
 
-public class StdCard extends Card{
+import it.polimi.ingsw.model.enums.Resource;
+
+public class StdCard extends ColoredCard{
+    private final boolean point;
+    public StdCard(int id, Corner[] frontCorners, Resource backResource, boolean point) {
+        super(id, frontCorners, backResource);
+        this.point = point;
+    }
+
+    public boolean isPoint() {
+        return point;
+    }
 }
