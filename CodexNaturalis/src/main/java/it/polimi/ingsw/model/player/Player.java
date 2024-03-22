5087c03f06ea8fee04901ef02e45b0c643e60366
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-03-23 00:47:05 +0100

    feat: in Player added missing getter/ setter, fixed constructor

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/player/Player.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/player/Player.java
@@ -22,10 +22,22 @@ public final class Player {
     public Player(String username, Game currentGame) {
         this.username = username;
         this.game = currentGame;
+        this.score = 0;
+        for (Resource r : Resource.values()){
+            playerResources.put(r, 0);
+        }
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public int[][] getBoard() {
+        return board;
     }
 
     public Card[] getHand(){
@@ -34,6 +46,14 @@ public final class Player {
 
     public Objective getHiddenObjective(){
         return hiddenObjective;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setHiddenObjective(Objective hiddenObjective) {
+        this.hiddenObjective = hiddenObjective;
     }
 
     public void drawFirstHand(){
