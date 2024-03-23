38b4c036036a59ecf5fe9c047c3d6a374416a41e
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-03-23 19:50:31 +0100

    feat: added NONCOVERABLE in Resource

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/enums/Resource.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/enums/Resource.java
@@ -1,5 +1,5 @@
 package it.polimi.ingsw.model.enums;
 
 public enum Resource {
-    MUSHROOM, LEAF, WOLF, BUTTERFLY, INK, QUILL, SCROLL, NONE
+    MUSHROOM, LEAF, WOLF, BUTTERFLY, INK, QUILL, SCROLL, NONE, NONCOVERABLE
 }
