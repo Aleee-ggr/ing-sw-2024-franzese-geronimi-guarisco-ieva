6669b76f83a21aaf3838c8f625cd09fff639f26f
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-03-23 19:30:15 +0100

    feat: created GoldCard prototype

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/GoldCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/GoldCard.java
@@ -1,8 +1,24 @@
 package it.polimi.ingsw.model.cards;
 
-public class GoldCard extends Card{
-    public GoldCard(int id, Corner[] frontCorners) {
-        //TODO actual constructor
-        super(id, frontCorners);
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.enums.Resource;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+public class GoldCard extends ColoredCard{
+    private final ConcurrentHashMap<Resource, Integer> requirements = new ConcurrentHashMap<>();
+    private final Function<Game, Integer> calculateScore;
+    public GoldCard(int id, Corner[] frontCorners, Resource backResource, Function<Game, Integer> calculateScore) {
+        super(id, frontCorners, backResource);
+        this.calculateScore = calculateScore;
+    }
+
+    public Integer getCalculateScore(Game game) {
+        return this.calculateScore.apply(game);
+    }
+
+    public ConcurrentHashMap<Resource, Integer> getRequirements() {
+        return new ConcurrentHashMap<>(requirements);
     }
 }
