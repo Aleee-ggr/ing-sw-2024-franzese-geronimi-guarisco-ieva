9a4f979357ec635072a5f99fb58f40fa0b2ef2a4
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-03-24 22:24:29 +0100

    test: added drawFromDeck test

--- CodexNaturalis/src/test/java/it/polimi/ingsw/DeckTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/DeckTest.java
@@ -1,15 +1,23 @@
 package it.polimi.ingsw;
 
 import it.polimi.ingsw.helpers.DeckFactory;
+import it.polimi.ingsw.helpers.parsers.GoldCardParser;
+import it.polimi.ingsw.helpers.parsers.StdCardParser;
 import it.polimi.ingsw.model.cards.Deck;
 import it.polimi.ingsw.model.cards.GoldCard;
 import it.polimi.ingsw.model.cards.StdCard;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class DeckTest {
     private Deck<GoldCard> goldDeck;
     private Deck<StdCard> stdDeck;
+    StdCardParser stdParser = new StdCardParser();
+    GoldCardParser goldParser = new GoldCardParser();
+    Path cardJsonPath = Path.of(GameConsts.cardJsonPath);
 
     @Test
     public void isEmpty() {
@@ -18,5 +26,21 @@ public class DeckTest {
 
         Assert.assertTrue(goldDeck.isEmpty());
         Assert.assertTrue(stdDeck.isEmpty());
+    }
+
+    @Test
+    public void drawFromDeck() {
+        try {
+            stdParser.readFile(cardJsonPath);
+            goldParser.readFile(cardJsonPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Deck<StdCard> stdDeckFull = stdParser.parse();
+        Deck<GoldCard> goldDeckFull = goldParser.parse();
+
+        Assert.assertNotNull(stdDeckFull.draw());
+        Assert.assertNotNull(goldDeckFull.draw());
     }
 }
