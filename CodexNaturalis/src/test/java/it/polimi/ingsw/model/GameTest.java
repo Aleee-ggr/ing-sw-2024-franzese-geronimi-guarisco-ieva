baa59e063bc1b60d883d8c716b0d033a781aa757
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-03-25 12:38:25 +0100

    tests: added tests for addPlayer method of Game class

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/model/GameTest.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.GameConsts;
+import it.polimi.ingsw.model.exceptions.ExistingUsernameException;
+import it.polimi.ingsw.model.exceptions.TooManyPlayersException;
+import it.polimi.ingsw.model.player.Player;
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class GameTest {
+    private final Game game = new Game(null);
+    private final ConcurrentHashMap<String, Player> players = new ConcurrentHashMap<>();
+    @Test
+    public void NormalAdd() throws ExistingUsernameException, TooManyPlayersException {
+        for(int i = 0; i < GameConsts.maxPlayersNum; i++){
+            game.addPlayer("user"+i);
+        }
+        assertNotNull(game.getPlayers());
+        assertEquals(GameConsts.maxPlayersNum ,game.getPlayers().size());
+    }
+
+    @Test (expected = ExistingUsernameException.class)
+    public void DuplicatePlayer() throws ExistingUsernameException, TooManyPlayersException {
+        game.addPlayer("user");
+        game.addPlayer("user");
+    }
+}
