9f7d9e38062dbee1168d4de847d8c877998597c2
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-26 13:44:31 +0100

    feat: added emptyStdCard method

--- CodexNaturalis/src/test/java/it/polimi/ingsw/helpers/CardFactory.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/helpers/CardFactory.java
@@ -1,9 +1,14 @@
 package it.polimi.ingsw.helpers;
 
 import it.polimi.ingsw.model.cards.GoldCard;
+import it.polimi.ingsw.model.cards.StdCard;
 
 public abstract class CardFactory {
     public static GoldCard emptyGoldCard() {
         return new GoldCard(-1, null, null, null, null);
+    }
+
+    public static StdCard emptyStdCard() {
+        return new StdCard(-1, null, null, false);
     }
 }
