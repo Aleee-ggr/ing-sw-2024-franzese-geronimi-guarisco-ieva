c379dc38b1bb0ca5e19e46669664ecb5dabc3b57
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-27 13:50:18 +0100

    docs: added javadocs to stdCard class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StdCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StdCard.java
@@ -9,11 +9,24 @@ import it.polimi.ingsw.model.enums.Resource;
  */
 public class StdCard extends ColoredCard{
     private final boolean point;
+
+    /**
+     * Constructor for the StdCard class.
+     * @param id Unique identifier of the card.
+     * @param frontCorners Array of corners on the front side of the card.
+     * @param backResource Resource on the back side of the card.
+     * @param point Indicates whether the card has one point or not.
+     */
     public StdCard(int id, Corner[] frontCorners, Resource backResource, boolean point) {
         super(id, frontCorners, backResource);
         this.point = point;
     }
 
+    /**
+     * Checks whether the card has points.
+     * StdCard can only have one point or zero.
+     * @return True if the card has points, otherwise false.
+     */
     public boolean isPoint() {
         return point;
     }
