4059abab21f616a4e39eb0af245c8481e60cebd4
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-27 13:58:05 +0100

    docs: added javadocs to startingCard class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StartingCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/StartingCard.java
@@ -15,16 +15,32 @@ import java.util.Arrays;
 public class StartingCard extends Card{
     private final Corner[] backCorners;
     private final ArrayList<Resource> frontResources;
+
+    /**
+     * Constructor for the StartingCard class.
+     * @param id Unique identifier of the card.
+     * @param frontCorners Array of corners on the front side of the card.
+     * @param backCorners Array of corners on the back side of the card.
+     * @param frontResources List of resources in the middle of the card's front side.
+     */
     public StartingCard(int id, Corner[] frontCorners, Corner[] backCorners, ArrayList<Resource> frontResources) {
         super(id, frontCorners);
         this.backCorners = backCorners;
         this.frontResources = frontResources;
     }
 
+    /**
+     * Retrieves the back corners of the card.
+     * @return Array of back corners of the card.
+     */
     public Corner[] getBackCorners() {
         return Arrays.copyOf(backCorners, GameConsts.numCorners);
     }
 
+    /**
+     * Retrieves the front resources in the middle of the card.
+     * @return List of front resources in the middle of the card.
+     */
     public ArrayList<Resource> getFrontResources() {
         return frontResources;
     }
