a5d3ad60c677f28ec567a451acabfe992da6e44c
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-03-27 15:45:10 +0100

    feat: created FullDeck class to return deck filled by cards from  cards.json

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/FullDeck.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.model.cards;
+
+import it.polimi.ingsw.GameConsts;
+import it.polimi.ingsw.helpers.parsers.GoldCardParser;
+import it.polimi.ingsw.helpers.parsers.ObjectiveParser;
+import it.polimi.ingsw.helpers.parsers.StartingParser;
+import it.polimi.ingsw.helpers.parsers.StdCardParser;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.objectives.Objective;
+
+import java.nio.file.Path;
+
+public class FullDeck {
+
+    private static Deck<GoldCard> FullGoldDeck;
+    private static Deck<StdCard> FullStdDeck;
+    private static final Path cardJsonPath = Path.of(GameConsts.cardJsonPath);
+
+    public FullDeck(){
+        BuildGoldDeck();
+        BuildStdDeck();
+    }
+
+    public static Deck<GoldCard> getFullGoldDeck() {
+        return FullGoldDeck;
+    }
+
+    public static Deck<StdCard> getFullStdDeck() {
+        return FullStdDeck;
+    }
+
+    //TODO: better error handling, also for parsing
+    public static void BuildGoldDeck() {
+        GoldCardParser goldParser = new GoldCardParser();
+        try {
+            goldParser.readFile(cardJsonPath);
+        } catch (Exception e) {
+            System.out.println("read-file Exception");
+        }
+        FullGoldDeck = goldParser.parse();
+    }
+
+    public static void BuildStdDeck() {
+        StdCardParser stdParser = new StdCardParser();
+        try {
+            stdParser.readFile(cardJsonPath);
+        } catch (Exception e) {
+            System.out.println("read-file Exception");
+        }
+        FullStdDeck = stdParser.parse();
+    }
+}
