9136d94c1b591701e6813721b16ff525f9a9294c
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-27 15:06:17 +0100

    docs: added javadocs to tooManyPlayersException class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/exceptions/TooManyPlayersException.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exceptions/TooManyPlayersException.java
@@ -1,6 +1,15 @@
 package it.polimi.ingsw.model.exceptions;
 
+/**
+ * Exception thrown when attempting to add more players than allowed.
+ * @author Alessio Guarisco
+ */
 public class TooManyPlayersException extends Exception {
+
+    /**
+     * Constructs a new TooManyPlayersException with the specified error message.
+     * @param message The detail message of the exception.
+     */
     public TooManyPlayersException(String message) {
         super(message);
     }
