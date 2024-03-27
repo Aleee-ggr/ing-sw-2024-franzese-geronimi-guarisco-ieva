38aa1675b7b09011f2d3533e4a514ab2ed6174c2
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-27 14:48:04 +0100

    docs: added javadocs to deck class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Deck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Deck.java
@@ -3,20 +3,36 @@ package it.polimi.ingsw.model.cards;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Represents a generic deck of cards.
+ * @param <CardType> The type of cards contained in the deck.
+ * @author Daniele Ieva
+ */
+
 public class Deck<CardType> {
     private final ArrayList<CardType> cards;
 
+    /**
+     * Constructor for the Deck class.
+     * @param cards The list of cards to initialize the deck with.
+     */
     public Deck(ArrayList<CardType> cards) {
         this.cards = cards;
     }
 
+    /**
+     * Shuffles the cards in the deck.
+     */
     public void shuffle() {
         synchronized (cards) {
-
             Collections.shuffle(cards);
         }
     }
 
+    /**
+     * Draws a card from the deck.
+     * @return The card drawn from the deck, or null if the deck is empty.
+     */
     public CardType draw() {
         synchronized (cards) {
             if (!cards.isEmpty()) {
@@ -26,10 +42,18 @@ public class Deck<CardType> {
         }
     }
 
+    /**
+     * Checks if the deck is empty.
+     * @return True if the deck is empty, otherwise false.
+     */
     public boolean isEmpty() {
         return cards.isEmpty();
     }
 
+    /**
+     * Retrieves the list of cards in the deck.
+     * @return A copy of the list of cards in the deck.
+     */
     public ArrayList<CardType> getCards() {
         return new ArrayList<>(cards);
     }
