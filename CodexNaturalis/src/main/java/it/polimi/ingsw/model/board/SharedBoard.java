b57ec34c10fa82a7fb87d7a3a9366e99bd2ac803
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-27 14:58:24 +0100

    docs: added javadocs to sharedBoard class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/board/SharedBoard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/board/SharedBoard.java
@@ -12,6 +12,12 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Represents the shared board in the game.
+ * The shared board contains information about objectives, scores, visible cards, and decks of cards.
+ * @author Daniele Ieva
+ */
+
 public class SharedBoard {
     private final Objective[] objectives = new Objective[GameConsts.globalObjectives];
     private final ConcurrentHashMap<Player, Integer> scoreMap = new ConcurrentHashMap<>();
@@ -19,15 +25,29 @@ public class SharedBoard {
     private final Deck<StdCard> stdDeck;
     private final Card[] visibleCards = new Card[4];
 
+    /**
+     * Constructor for the SharedBoard class.
+     * @param goldDeck The deck of gold cards.
+     * @param stdDeck The deck of standard cards.
+     */
     public SharedBoard(Deck<GoldCard> goldDeck, Deck<StdCard> stdDeck) {
         this.goldDeck = goldDeck;
         this.stdDeck = stdDeck;
     }
 
+    /**
+     * Retrieves the visible cards on the shared board.
+     * @return An array containing the visible cards.
+     */
     public Card[] getVisibleCards() {
         return Arrays.copyOf(visibleCards, GameConsts.visibleCards);
     }
 
+    /**
+     * Substitutes a card on the shared board at the specified position with a new card from the deck.
+     * @param position The position of the card to be substituted.
+     * @param isGold Indicates whether the card to be substituted is a gold card.
+     */
     public void substituteCard(int position, boolean isGold) {
         if (isGold) {
             if (!goldDeck.isEmpty()){
@@ -51,6 +71,10 @@ public class SharedBoard {
         }
     }
 
+    /**
+     * Checks if there are no cards left in the decks and on the shared board.
+     * @return True if there are no cards left, otherwise false.
+     */
     public boolean areCardsOver() {
         if (!goldDeck.isEmpty() || !stdDeck.isEmpty()) {
             return false;
@@ -65,6 +89,11 @@ public class SharedBoard {
         return true;
     }
 
+    /**
+     * Draws a card from the specified deck.
+     * @param isGold Indicates whether the card should be drawn from the gold card deck.
+     * @return The drawn card, or null if the deck is empty.
+     */
     public Card drawDeck(boolean isGold) {
         if (isGold && !goldDeck.isEmpty()) {
             return goldDeck.draw();
@@ -76,6 +105,11 @@ public class SharedBoard {
         return null;
     }
 
+    /**
+     * Retrieves the card at the specified position from the visible cards.
+     * @param position The position of the card to retrieve.
+     * @return The card at the specified position, or null if no card is present.
+     */
     public Card drawVisible(int position) {
         if (visibleCards[position] != null) {
             return visibleCards[position];
@@ -84,10 +118,18 @@ public class SharedBoard {
         return null;
     }
 
+    /**
+     * Retrieves a copy of the score map.
+     * @return A copy of the score map.
+     */
     public Map<Player, Integer> getScore() {
         return new ConcurrentHashMap<>(scoreMap); // Copy the map instead of returning the object itself
     }
 
+    /**
+     * Retrieves the global objectives on the shared board.
+     * @return An array containing the global objectives.
+     */
     public Objective[] getGlobalObjectives() {
         return Arrays.copyOf(objectives, GameConsts.globalObjectives);
     }
