5cf4912f5e02e7e401dcd30846d91ad8688da2bc
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-27 13:31:25 +0100

    docs: added javadocs to coloredCard class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/ColoredCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/ColoredCard.java
@@ -10,11 +10,22 @@ import it.polimi.ingsw.model.enums.Resource;
 
 public abstract class ColoredCard extends Card{
     private final Resource backResource;
+
+    /**
+     * Constructor for the ColoredCard class.
+     * @param id Unique identifier of the card.
+     * @param frontCorners Array of corners on the front side of the card.
+     * @param backResource Resource on the back side of the card.
+     */
     public ColoredCard(int id, Corner[] frontCorners, Resource backResource) {
         super(id, frontCorners);
         this.backResource = backResource;
     }
 
+    /**
+     * Retrieves the resource on the back side of the card.
+     * @return The resource on the back side of the card.
+     */
     public Resource getBackResource() {
         return backResource;
     }
