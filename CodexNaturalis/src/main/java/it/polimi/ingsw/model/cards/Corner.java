e83e52d5a4af6957a8d9ce408a75ef63f5e8cbdc
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-27 14:27:28 +0100

    docs: added javadocs to corner class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Corner.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/Corner.java
@@ -13,15 +13,28 @@ public class Corner {
     private final Resource cornerResource;
     private final boolean coverable;
 
+    /**
+     * Constructor for the Corner class.
+     * @param cornerResource The resource located in the corner.
+     * @param coverable Indicates whether the corner can be covered.
+     */
     public Corner(Resource cornerResource, boolean coverable){
         this.cornerResource = cornerResource;
         this.coverable = coverable;
     }
 
+    /**
+     * Retrieves the resource located in the corner.
+     * @return The resource located in the corner.
+     */
     public Resource getCornerResource() {
         return cornerResource;
     }
 
+    /**
+     * Checks whether the corner can be covered.
+     * @return True if the corner can be covered, false otherwise.
+     */
     public boolean isCoverable() {
         return coverable;
     }
