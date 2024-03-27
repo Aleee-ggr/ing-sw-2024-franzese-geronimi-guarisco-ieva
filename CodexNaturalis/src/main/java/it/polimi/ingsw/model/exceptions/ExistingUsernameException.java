75a507af7ef70fddb5ccd2c38c711b99e3a044a7
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-03-27 15:03:55 +0100

    docs: added javadocs to existingUsernameException class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/exceptions/ExistingUsernameException.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exceptions/ExistingUsernameException.java
@@ -1,6 +1,15 @@
 package it.polimi.ingsw.model.exceptions;
 
+/**
+ * Exception thrown when attempting to create a new player with an existing username.
+ * @author Alessio Guarisco
+ */
 public class ExistingUsernameException extends Exception{
+
+    /**
+     * Constructs a new ExistingUsernameException with the specified error message.
+     * @param message The detail message of the exception.
+     */
     public ExistingUsernameException(String message) {
         super(message);
     }
