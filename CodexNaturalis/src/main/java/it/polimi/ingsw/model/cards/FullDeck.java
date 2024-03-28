1d9d12971318a4a29154067bf8d9677b6399625b
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-03-28 16:28:29 +0100

    feat: created Full Decks of Objectives and Starting Cards

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/FullDeck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/cards/FullDeck.java
@@ -2,7 +2,12 @@ package it.polimi.ingsw.model.cards;
 
 import it.polimi.ingsw.GameConsts;
 import it.polimi.ingsw.helpers.parsers.GoldCardParser;
+import it.polimi.ingsw.helpers.parsers.ObjectiveParser;
+import it.polimi.ingsw.helpers.parsers.StartingParser;
 import it.polimi.ingsw.helpers.parsers.StdCardParser;
+
+import it.polimi.ingsw.model.objectives.Objective;
+
 
 import java.nio.file.Path;
 
@@ -10,6 +15,8 @@ public class FullDeck {
 
     private static Deck<GoldCard> FullGoldDeck;
     private static Deck<StdCard> FullStdDeck;
+    private static Deck<Objective> FullObjDeck;
+    private static Deck<StartingCard> FullStartingDeck;
     private static final Path cardJsonPath = Path.of(GameConsts.cardJsonPath);
 
     public FullDeck(){
@@ -25,24 +32,52 @@ public class FullDeck {
         return FullStdDeck;
     }
 
-    //TODO: better error handling, also for parsing
+    public static Deck<Objective> getFullObjDeck() {
+        return FullObjDeck;
+    }
+
+    public static Deck<StartingCard> getFullStartingDeck() {
+        return FullStartingDeck;
+    }
+
+
     public static void BuildGoldDeck() {
         GoldCardParser goldParser = new GoldCardParser();
         try {
             goldParser.readFile(cardJsonPath);
+            FullGoldDeck = goldParser.parse();
         } catch (Exception e) {
-            System.out.println("read-file Exception");
+            System.out.println("Exception");
         }
-        FullGoldDeck = goldParser.parse();
     }
 
     public static void BuildStdDeck() {
         StdCardParser stdParser = new StdCardParser();
         try {
             stdParser.readFile(cardJsonPath);
+            FullStdDeck = stdParser.parse();
         } catch (Exception e) {
-            System.out.println("read-file Exception");
+            System.out.println("Exception");
         }
-        FullStdDeck = stdParser.parse();
+    }
+
+    public static void BuildObjDeck() {
+        ObjectiveParser objParser = new ObjectiveParser();
+        try {
+            objParser.readFile(cardJsonPath);
+            FullObjDeck = objParser.parse();
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
+
+    public static void BuildStartingDeck() {
+        StartingParser startingParser = new StartingParser();
+        try {
+            startingParser.readFile(cardJsonPath);
+            FullStartingDeck = startingParser.parse();
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
     }
 }
