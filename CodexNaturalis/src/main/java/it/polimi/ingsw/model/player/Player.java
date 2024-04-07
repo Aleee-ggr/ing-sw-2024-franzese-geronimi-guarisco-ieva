37799bf73fa30552e288397337246e4878334a63
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-04-07 19:13:28 +0200

    revert: reverted last commit

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/player/Player.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/player/Player.java
@@ -151,7 +151,7 @@ public class Player {
      * */
     public void drawFirstHand(){
         this.hand = new Card[GameConsts.firstHandDim];
-        for (int i = 0; i < GameConsts.fistHandStdNum; i++){
+        for (int i = 0; i < GameConsts.fistHandStdNum - 1; i++){
             this.hand[i] = game.getGameBoard().drawDeck(false);
         }
         for (int i = GameConsts.fistHandStdNum; i < GameConsts.firstHandDim; i++){
