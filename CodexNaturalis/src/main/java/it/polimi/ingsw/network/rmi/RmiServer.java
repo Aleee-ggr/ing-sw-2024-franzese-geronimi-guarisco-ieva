fe161e46c152746013a2424bcd0c63607e7c2ef9
Daniele Ieva <daniele.ieva@hotmail.com>
2024-04-15 11:13:22 +0200

    feat: added rmi template

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/RmiServer.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/RmiServer.java
@@ -1,6 +1,36 @@
 package it.polimi.ingsw.network.rmi;
 
+import it.polimi.ingsw.controller.threads.Shared;
+import it.polimi.ingsw.controller.threads.ThreadMessage;
 import it.polimi.ingsw.network.Server;
 
-public class RmiServer implements Server {
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class RmiServer implements Server, RmiServerInterface {
+    private static final String name="rmiServer";
+    private final Registry registry;
+    private final Map<UUID, Shared<ThreadMessage>> threadMessages = new HashMap<>();
+
+    public RmiServer(int port) throws RemoteException {
+        System.out.println("Starting server...");
+        RmiServerInterface stub = (RmiServerInterface) UnicastRemoteObject.exportObject(this, port);
+        registry = LocateRegistry.createRegistry(1099);
+        registry.rebind(name, stub);
+        System.out.println("Started!");
+    }
+
+    public void stop() {
+        try {
+            registry.unbind(name);
+        } catch (NotBoundException | RemoteException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/RmiServerInterface.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.network.rmi;
+
+import java.rmi.Remote;
+
+public interface RmiServerInterface extends Remote {
+}
