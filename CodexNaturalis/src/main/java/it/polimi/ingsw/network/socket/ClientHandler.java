1a328045a1811e8d1f79d5735de2728d1771fa4e
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-04-16 23:12:04 +0200

    docs: added javadoc for ClientHandler class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/ClientHandler.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/ClientHandler.java
@@ -5,11 +5,22 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+/**
+ * The ClientHandler class manages the communication between a client and a server using sockets.
+ * It provides methods to send and receive objects over the network.
+ * @author Samuele Franzese
+ */
 public class ClientHandler{
     private Socket socket;
     private ObjectInputStream input;
     private ObjectOutputStream output;
-    
+
+    /**
+     * Constructs a new ClientHandler object with the specified socket connection.
+     *
+     * @param socket the socket connection to the client
+     * @throws IOException if an I/O error occurs while initializing the input and output streams
+     */
     public ClientHandler(Socket socket) throws IOException {
         this.socket = socket;
         this.input = new ObjectInputStream(socket.getInputStream());
