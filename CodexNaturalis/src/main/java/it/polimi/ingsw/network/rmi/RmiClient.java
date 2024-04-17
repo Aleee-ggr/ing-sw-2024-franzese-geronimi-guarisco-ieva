d30be61638fb00c38152958e7630f10ea9ee11db
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-04-17 08:20:56 +0200

    feat: RmiClient connection implementation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/RmiClient.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/RmiClient.java
@@ -1,13 +1,29 @@
 package it.polimi.ingsw.network.rmi;
 
 
+import it.polimi.ingsw.helpers.exceptions.network.ServerConnectionException;
 import it.polimi.ingsw.network.Client;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.UUID;
 
-public class RmiClient extends Client {
+public class RmiClient extends Client{
+
+    private RmiServerInterface server;
 
     public RmiClient(UUID gameId, String playerUsername, String serverAddress, int serverPort) {
         super(gameId, playerUsername, serverAddress, serverPort);
+        try {
+            Registry registry = LocateRegistry.getRegistry();
+            server = (RmiServerInterface) registry.lookup("rmiServer");
+        } catch (RemoteException e) {
+            System.err.println("Client exception: " + e.toString());
+        } catch (NotBoundException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
