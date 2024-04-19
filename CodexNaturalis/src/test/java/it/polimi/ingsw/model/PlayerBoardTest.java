a3d728e822ce5f8ba3eb677b8aa8c6e8c8d86c3b
Daniele Ieva <daniele.ieva@hotmail.com>
2024-04-19 12:56:05 +0200

    test: completed dfs test

--- CodexNaturalis/src/test/java/it/polimi/ingsw/model/PlayerBoardTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/model/PlayerBoardTest.java
@@ -1,13 +1,19 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.GameConsts;
 import it.polimi.ingsw.helpers.MockPlayer;
+import it.polimi.ingsw.model.board.Coordinates;
 import it.polimi.ingsw.model.board.PlayerBoard;
 import it.polimi.ingsw.model.cards.FullDeck;
 import it.polimi.ingsw.model.player.Player;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 
 public class PlayerBoardTest {
     PlayerBoard board;
@@ -22,5 +28,9 @@ public class PlayerBoardTest {
     @Test
     public void checkPositionsIfDisconnected_test_start() {
         board.checkPositionsIfDisconnected();
+        Set<Coordinates> expected = new HashSet<>(GameConsts.centralPoint.getNeighbors());
+        Set<Coordinates> actual = board.getValidPlacements();
+        assertEquals(expected, actual);
+
     }
 }
