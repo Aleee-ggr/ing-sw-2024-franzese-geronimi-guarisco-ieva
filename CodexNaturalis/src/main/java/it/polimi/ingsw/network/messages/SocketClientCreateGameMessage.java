65ab51558cf6dc9440bdcb40a6302fb9fac5a5fa
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-04-21 23:30:45 +0200

    feat: added numPlayers attribute and getter

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/SocketClientCreateGameMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/SocketClientCreateGameMessage.java
@@ -8,13 +8,19 @@ package it.polimi.ingsw.network.messages;
  * @author gloriageronimi
  */
 public class SocketClientCreateGameMessage extends Message{
-
+    private final int numPlayers;
     /**
      * Constructor for the SocketClientCreateGameMessage class.
      *
      * @param username The username of the client sending the message.
+     * @param numPlayers The number of players for the game.
      */
-    public SocketClientCreateGameMessage(String username) {
+    public SocketClientCreateGameMessage(String username, int numPlayers) {
         this.username = username;
+        this.numPlayers = numPlayers;
+    }
+
+    public int getNumPlayers() {
+        return numPlayers;
     }
 }
