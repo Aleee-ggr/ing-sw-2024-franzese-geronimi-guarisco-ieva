ca21c497acd5f16e3d9bbea83d4863f90d1ff8a9
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-04-22 23:51:15 +0200

    feat: created SocketClientPlaceCardMessage class

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/SocketClientPlaceCardMessage.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.network.messages;
+
+import it.polimi.ingsw.model.board.Coordinates;
+
+import java.util.UUID;
+
+public class SocketClientPlaceCardMessage extends Message{
+    private final Coordinates coordinates;
+    private final int cardId;
+    private final UUID gameUUID;
+
+    public SocketClientPlaceCardMessage(String username, Coordinates coordinates, int cardId, UUID gameUUID) {
+        this.username = username;
+        this.coordinates = coordinates;
+        this.cardId = cardId;
+        this.gameUUID = gameUUID;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public UUID getGameUUID() {
+        return gameUUID;
+    }
+}
