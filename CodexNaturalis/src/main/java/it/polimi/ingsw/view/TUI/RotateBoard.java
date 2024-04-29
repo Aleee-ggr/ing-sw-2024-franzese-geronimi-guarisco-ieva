3c4b6b6244122ab76558dd974a41156f48f95ba4
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-04-29 17:56:51 +0200

    feat: add MiniBoard and RotateBoard class

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/RotateBoard.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.view.TUI;
+
+import it.polimi.ingsw.model.board.Coordinates;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RotateBoard {
+    private static final Integer rotationAngle = 45;
+    public static Map<Coordinates, Integer> rotateBoard(Map<Coordinates, Integer> board) {
+        Map<Coordinates, Integer> rotatedBoard= new HashMap<>();
+
+        for (Coordinates coordinate : board.keySet()) {
+            int x = coordinate.x();
+            int y = coordinate.y();
+            int value = board.get(coordinate);
+
+            // matrix rotation with dilation
+            double radians = Math.toRadians(rotationAngle);
+            int rotated_x = (int) Math.round((x * Math.cos(radians) - y * Math.sin(radians)) * Math.sqrt(2));
+            int rotated_y = (int) Math.round((x * Math.sin(radians) + y * Math.cos(radians)) * Math.sqrt(2));
+
+            rotatedBoard.put(new Coordinates(rotated_x, rotated_y), value);
+        }
+        return rotatedBoard;
+    }
+}
--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/components/MiniBoard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/components/MiniBoard.java
@@ -1,12 +1,43 @@
 package it.polimi.ingsw.view.TUI.components;
 
+import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.board.Coordinates;
+import it.polimi.ingsw.model.cards.Card;
+import it.polimi.ingsw.model.cards.ColoredCard;
+import it.polimi.ingsw.view.TUI.RotateBoard;
 
 import java.util.Map;
 
+
 public class MiniBoard {
-    private String[][] boardToPrint = new String[8][7];
-    public MiniBoard(Map<Coordinates, Integer> board) {
-        
+    private final int boardHeight = 7;
+    private final int boardWidth = 9;
+    private final Character[][] boardToPrint = new Character[boardWidth][boardHeight];
+
+    public Character[][] get(Map<Coordinates, Integer> board) {
+        Map<Coordinates, Integer> rotatedBoard = RotateBoard.rotateBoard(board);
+        for (int y = 0; y < boardHeight; y++){
+            for(int x = 0; x < boardWidth; x++){
+                Integer cardId = rotatedBoard.get(getBoardCoords(x, y));
+                if(cardId == null){
+                    boardToPrint[x][y] = ' ';
+                } else {
+                    Card card = Game.getCardByID(cardId);
+                    if (card.isColored()){
+                        ColoredCard coloredCard = (ColoredCard) card;
+                        boardToPrint[x][y] = coloredCard.getBackResource().toChar();
+                    } else {
+                        boardToPrint[x][y] = 'S';
+                    }
+                }
+            }
+        }
+        return boardToPrint;
+    }
+
+    private Coordinates getBoardCoords(int x, int y){
+        int newX = x - (boardWidth - 1) / 2;
+        int newY = (boardHeight - 1) / 2 - y;
+        return new Coordinates(newX, newY);
     }
 }
