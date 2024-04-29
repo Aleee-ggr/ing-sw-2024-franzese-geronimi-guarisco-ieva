a71e35073bc9903d0d16031e2d2b27fe8975c363
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-04-29 18:38:32 +0200

    feat: added and changed thread messages

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/threads/ThreadMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/threads/ThreadMessage.java
@@ -3,10 +3,7 @@ package it.polimi.ingsw.controller.threads;
 import it.polimi.ingsw.model.board.Coordinates;
 import it.polimi.ingsw.model.enums.Resource;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -14,6 +11,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * It has different ThreadMessages for each message needed.
  * @author Daniele Ieva
  * @author Alessio Guarisco
+ * @author Gloria Geronimi
+ * @author Samuele Franzese
  * */
 public record ThreadMessage(Status status, String player, String type, String[] args, UUID messageUUID) {
     public static ThreadMessage create(String username, Integer playerNum, UUID gameId){
@@ -35,6 +34,18 @@ public record ThreadMessage(Status status, String player, String type, String[] 
                 username,
                 "join",
                 null,
+                UUID.randomUUID()
+        );
+    }
+
+    public static ThreadMessage choosePersonalObjective (String username, Integer objectiveId) {
+        return new ThreadMessage(
+                Status.REQUEST,
+                username,
+                "choosePersonalObjective",
+                new String[]{
+                        objectiveId.toString()
+                },
                 UUID.randomUUID()
         );
     }
@@ -115,6 +126,16 @@ public record ThreadMessage(Status status, String player, String type, String[] 
         );
     }
 
+    public static ThreadMessage getStartingObjectives(String username) {
+        return new ThreadMessage(
+                Status.REQUEST,
+                username,
+                "getStartingObjectives",
+                null,
+                UUID.randomUUID()
+        );
+    }
+
     public static ThreadMessage getPlayerResources(String username) {
         return new ThreadMessage(
                 Status.REQUEST,
@@ -155,21 +176,31 @@ public record ThreadMessage(Status status, String player, String type, String[] 
         );
     }
 
-    public static ThreadMessage getPlayersBoards(String username) {
+    public static ThreadMessage getBoard(String username) {
         return new ThreadMessage(
                 Status.REQUEST,
                 username,
-                "getPlayersBoards",
+                "getBoard",
                 null,
                 UUID.randomUUID()
         );
     }
 
-    public static ThreadMessage choosePersonalObjective(String username) {
+    public static ThreadMessage getHandColor(String username) {
         return new ThreadMessage(
                 Status.REQUEST,
                 username,
-                "choosePersonalObjective",
+                "getHandColor",
+                null,
+                UUID.randomUUID()
+        );
+    }
+
+    public static ThreadMessage getLastPlacedCards(String username) {
+        return new ThreadMessage(
+                Status.REQUEST,
+                username,
+                "getLastPlacedCards",
                 null,
                 UUID.randomUUID()
         );
@@ -182,7 +213,7 @@ public record ThreadMessage(Status status, String player, String type, String[] 
                 "genericError",
                 null,
                 messageUUID
-            );
+        );
     }
 
     public static ThreadMessage okResponse(String username, UUID messageUUID) {
@@ -192,7 +223,7 @@ public record ThreadMessage(Status status, String player, String type, String[] 
                 "ok",
                 null,
                 messageUUID
-            );
+        );
     }
 
     public static ThreadMessage drawResponse(String username, Integer cardId, UUID messageUUID) {
@@ -202,6 +233,18 @@ public record ThreadMessage(Status status, String player, String type, String[] 
                 "drawResponse",
                 new String[] {
                         cardId.toString()
+                },
+                messageUUID
+        );
+    }
+
+    public static ThreadMessage choosePersonalObjectiveResponse (String username, boolean correct, UUID messageUUID) {
+        return new ThreadMessage(
+                Status.OK,
+                username,
+                "choosePersonalObjectiveResponse",
+                new String[]{
+                        String.valueOf(correct)
                 },
                 messageUUID
         );
@@ -254,6 +297,23 @@ public record ThreadMessage(Status status, String player, String type, String[] 
         );
     }
 
+    public static ThreadMessage getLastPlacedCardsResponse(String username, Deque<Integer> lastPlacedCards, UUID messageUUID) {
+        String[] args = new String[lastPlacedCards.size()];
+        int index = 0;
+
+        for (Integer cardId : lastPlacedCards) {
+            args[index] = cardId.toString();
+        }
+
+        return new ThreadMessage(
+                Status.OK,
+                username,
+                "getLastPlacedCardsResponse",
+                args,
+                messageUUID
+        );
+    }
+
     public static ThreadMessage getHandResponse(String username, ArrayList<Integer> handIds, UUID messageUUID) {
         String[] args = new String[handIds.size()];
 
@@ -270,11 +330,27 @@ public record ThreadMessage(Status status, String player, String type, String[] 
         );
     }
 
-    public static ThreadMessage getVisibleCardResponse(String username, Integer[] cardId, UUID messageUUID) {
-        String[] args = new String[cardId.length];
+    public static ThreadMessage getHandColorResponse(String username, ArrayList<Resource> handColors, UUID messageUUID) {
+        String[] args = new String[handColors.size()];
 
-        for (int i = 0; i < cardId.length; i++) {
-            args[i] = cardId[i].toString();
+        for (int i = 0; i < handColors.size(); i++) {
+            args[i] = handColors.get(i).toString();
+        }
+
+        return new ThreadMessage(
+                Status.OK,
+                username,
+                "getHandColorResponse",
+                args,
+                messageUUID
+        );
+    }
+
+    public static ThreadMessage getVisibleCardResponse(String username, ArrayList<Integer> cardId, UUID messageUUID) {
+        String[] args = new String[cardId.size()];
+
+        for (int i = 0; i < cardId.size(); i++) {
+            args[i] = cardId.get(i).toString();
         }
 
         return new ThreadMessage(
@@ -286,17 +362,33 @@ public record ThreadMessage(Status status, String player, String type, String[] 
         );
     }
 
-    public static ThreadMessage getCommonObjectivesResponse(String username, Integer[] startingObjectivesIds, UUID messageUUID) {
-        String[] args = new String[startingObjectivesIds.length];
+    public static ThreadMessage getCommonObjectivesResponse(String username, ArrayList<Integer> startingObjectivesIds, UUID messageUUID) {
+        String[] args = new String[startingObjectivesIds.size()];
 
-        for (int i = 0; i < startingObjectivesIds.length; i++) {
-            args[i] = startingObjectivesIds[i].toString();
+        for (int i = 0; i < startingObjectivesIds.size(); i++) {
+            args[i] = startingObjectivesIds.get(i).toString();
         }
 
         return new ThreadMessage(
                 Status.OK,
                 username,
                 "getCommonObjectivesResponse",
+                args,
+                messageUUID
+        );
+    }
+
+    public static ThreadMessage getStartingObjectivesResponse(String username, ArrayList<Integer> startingObjectivesIds, UUID messageUUID) {
+        String[] args = new String[startingObjectivesIds.size()];
+
+        for (int i = 0; i < startingObjectivesIds.size(); i++) {
+            args[i] = startingObjectivesIds.get(i).toString();
+        }
+
+        return new ThreadMessage(
+                Status.OK,
+                username,
+                "getStartingObjectivesResponse",
                 args,
                 messageUUID
         );
@@ -320,17 +412,35 @@ public record ThreadMessage(Status status, String player, String type, String[] 
         );
     }
 
-    public static ThreadMessage getBackSideDecksResponse(String username, Integer[] backSideCardsIds, UUID messageUUID) {
-        String[] args = new String[backSideCardsIds.length];
+    public static ThreadMessage getBackSideDecksResponse(String username, ArrayList<Integer> backSideCardsIds, UUID messageUUID) {
+        String[] args = new String[backSideCardsIds.size()];
 
-        for (int i = 0; i < backSideCardsIds.length; i++) {
-            args[i] = backSideCardsIds[i].toString();
+        for (int i = 0; i < backSideCardsIds.size(); i++) {
+            args[i] = backSideCardsIds.get(i).toString();
         }
 
         return new ThreadMessage(
                 Status.OK,
                 username,
                 "getBackSideDecksResponse",
+                args,
+                messageUUID
+        );
+    }
+
+    public static ThreadMessage getBoardResponse(String username, HashMap<Coordinates, Integer> board, UUID messageUUID) {
+        String[] args = new String[board.size()];
+        int index = 0;
+
+        for (Map.Entry<Coordinates, Integer> entry : board.entrySet()) {
+            args[index] = entry.getKey().x().toString() + "," + entry.getKey().y().toString() + "," + entry.getValue().toString();
+            index++;
+        }
+
+        return new ThreadMessage(
+                Status.OK,
+                username,
+                "getBoardResponse",
                 args,
                 messageUUID
         );
