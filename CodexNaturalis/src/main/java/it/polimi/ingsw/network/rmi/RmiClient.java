497513caf16c396abb55c21d691fcd2ce1804d69
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-04-30 14:05:25 +0200

    docs: added javadocs to RmiClient

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/RmiClient.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/RmiClient.java
@@ -20,8 +20,14 @@ import java.util.UUID;
 
 /**
  * Represents a client using the RMI protocol.
- * Extends the abstract class Client that contains the specs of the general Client.
+ * Extends the abstract class Client, which contains the general specifications of a Client.
+ * This class connects to the RMI server and provides methods for interacting with the server,
+ * such as drawing and placing cards, joining games, and performing other game-related operations.
+ *
+ * @see Client
+ * @see RmiServerInterface
  * @author Alessio Guarisco
+ * @author Gloria Geronimi
  */
 
 public class RmiClient extends Client{
@@ -29,10 +35,13 @@ public class RmiClient extends Client{
     private RmiServerInterface server;
 
     /**
-     * Constructs a new RmiClient object with the specified player username, server address, and server port.
+     * Constructs a new RmiClient object with the specified player username, password, server address, and server port.
+     * Connects to the RMI server at the given address and port, and initializes the client.
      * @param playerUsername The username of the player.
-     * @param serverAddress  The address of the server.
-     * @param serverPort     The port of the server.
+     * @param password The password of the player.
+     * @param serverAddress The address of the server.
+     * @param serverPort The port of the server.
+     * @throws RuntimeException If an error occurs while connecting to the server.
      */
     public RmiClient(String playerUsername, String password, String serverAddress, int serverPort) {
         super(playerUsername, password, serverAddress, serverPort);
@@ -44,14 +53,20 @@ public class RmiClient extends Client{
         }
     }
 
+    /**
+     * Checks the credentials of the client with the server.
+     * @return True if the credentials are valid, false otherwise.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public boolean checkCredentials() throws RemoteException {
         return server.checkCredentials(data.getUsername(), data.getPassword());
     }
+
     /**
      * Draws a card from the server and adds it to the client's hand at the specified position.
      * @param position The index of the deck to draw from.
      * @throws ServerConnectionException If there is an issue connecting to the server.
-     * @throws RemoteException          If a remote communication error occurs.
+     * @throws RemoteException If a remote communication error occurs.
      */
     public void drawCard(int position) throws ServerConnectionException, RemoteException {
         int id = server.drawCard(this.gameId, data.getUsername(), position);
@@ -65,9 +80,9 @@ public class RmiClient extends Client{
     /**
      * Places a card from the client's hand onto the board at the specified coordinates.
      * @param coordinates The coordinates on the board where the card will be placed.
-     * @param cardId      The ID of the card to be placed.
+     * @param cardId The ID of the card to be placed.
      * @throws ServerConnectionException If there is an issue connecting to the server.
-     * @throws RemoteException          If a remote communication error occurs.
+     * @throws RemoteException If a remote communication error occurs.
      */
     public void placeCard(Coordinates coordinates, Integer cardId) throws ServerConnectionException, RemoteException {
         try{
@@ -83,7 +98,7 @@ public class RmiClient extends Client{
      * @param players The number of players for the new game.
      * @return The UUID of the newly created game.
      * @throws ServerConnectionException If there is an issue connecting to the server.
-     * @throws RemoteException          If a remote communication error occurs.
+     * @throws RemoteException If a remote communication error occurs.
      */
     public UUID newGame(int players) throws ServerConnectionException, RemoteException {
         return server.newGame(players);
@@ -93,86 +108,198 @@ public class RmiClient extends Client{
      * Requests to join a game with the specified UUID.
      * @param game The UUID of the game to join.
      * @throws ServerConnectionException If there is an issue connecting to the server.
-     * @throws RemoteException          If a remote communication error occurs.
+     * @throws RemoteException If a remote communication error occurs.
      */
     public void joinGame(UUID game) throws ServerConnectionException, RemoteException {
         server.join(game, data.getUsername());
         this.setGameId(game);
     }
 
+    /**
+     * Retrieves a map of player scores from the server.
+     * @return a map of player's usernames with theirs scores.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public HashMap<String, Integer> getScoreMap() throws ServerConnectionException, RemoteException {
         return Server.getScoreMap(this.gameId, data.getUsername());
     }
 
+    /**
+     * Retrieves the client's hand from the server.
+     * @return A list of card IDs in the client's hand.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public ArrayList<Integer> getHand() throws ServerConnectionException, RemoteException {
         return Server.getHand(this.gameId, data.getUsername());
     }
 
+    /**
+     * Retrieves the common objectives from the server.
+     * @return A list of common objective IDs.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public ArrayList<Integer> getCommonObjectives() throws ServerConnectionException, RemoteException {
         return Server.getCommonObjectives(this.gameId, data.getUsername());
     }
 
+    /**
+     * Retrieves the resources of a player from the server.
+     * @param usernameRequiredData The username of the player whose resources are to be retrieved.
+     * @return A map of resource types to quantities.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public HashMap<Resource, Integer> getPlayerResources(String usernameRequiredData) throws ServerConnectionException, RemoteException {
         return Server.getPlayerResources(this.gameId, data.getUsername(), usernameRequiredData);
     }
 
+    /**
+     * Retrieves the visible cards from the server.
+     * @return A list of visible card IDs.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public ArrayList<Integer> getVisibleCards() throws ServerConnectionException, RemoteException {
         return Server.getVisibleCards(this.gameId, data.getUsername());
     }
 
+    /**
+     * Retrieves the back side decks from the server.
+     * @return A list of back side deck IDs.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public ArrayList<Integer> getBackSideDecks() throws ServerConnectionException, RemoteException {
         return Server.getBackSideDecks(this.gameId, data.getUsername());
     }
 
+    /**
+     * Retrieves the valid placements from the server.
+     * @return A set of coordinates representing valid placements.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public Set<Coordinates> getValidPlacements() throws ServerConnectionException, RemoteException {
         return Server.getValidPlacements(this.gameId, data.getUsername());
     }
 
+    /**
+     * Retrieves the hand color of a player from the server.
+     * @param usernameRequiredData The username of the player whose hand color is to be retrieved.
+     * @return A list of resources representing the hand color.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public ArrayList<Resource> getPlayerHandColor(String usernameRequiredData) throws ServerConnectionException, RemoteException {
         return Server.getHandColor(this.gameId, data.getUsername(), usernameRequiredData);
     }
 
+    /**
+     * Sets the client's hand with data from the server.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void setClientHand() throws ServerConnectionException, RemoteException {
         data.setClientHand(Server.getHand(this.gameId, data.getUsername()));
     }
 
+    /**
+     * Sets the client's valid placements with data from the server.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void setValidPlacements() throws ServerConnectionException, RemoteException {
         data.setValidPlacements(Server.getValidPlacements(this.gameId, data.getUsername()));
     }
 
+    /**
+     * Updates the score of the specified player.
+     * @param username The username of the player whose score is to be updated.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void updateScore(String username) throws ServerConnectionException, RemoteException {
         data.updateScore(username, Server.getScoreMap(this.gameId, data.getUsername()).get(username));
     }
 
+    /**
+     * Sets the client's board with data from the server.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void setClientBoard() throws ServerConnectionException, RemoteException {
         data.setClientBoard(Server.getBoard(this.gameId, data.getUsername(), data.getUsername()));
     }
 
+    /**
+     * Updates the resources of the specified player.
+     * @param username The username of the player whose resources are to be updated.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void updatePlayerResources(String username) throws ServerConnectionException, RemoteException {
         data.updatePlayerResources(username, Server.getPlayerResources(this.gameId, data.getUsername(), username));
     }
 
+    /**
+     * Sets the board of the specified player with data from the server.
+     * @param username The username of the player whose board is to be set.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void setPlayerBoard(String username) throws ServerConnectionException, RemoteException {
         data.setPlayerBoard(username, Server.getBoard(this.gameId, data.getUsername(), username));
     }
 
+    /**
+     * Sets the hand color of the specified player with data from the server.
+     * @param username The username of the player whose hand color is to be set.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void setPlayerHandColor(String username) throws ServerConnectionException, RemoteException {
         data.setPlayerHandColor(username, Server.getHandColor(this.gameId, data.getUsername(), username));
     }
 
+    /**
+     * Allows the client to choose a starting objective.
+     * @param objectiveId The ID of the chosen objective.
+     * @return True if the choice is successful, false otherwise.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public boolean chooseStartingObjective(int objectiveId) throws  ServerConnectionException, RemoteException{
         return Server.choosePersonalObjective(this.gameId, data.getUsername(), objectiveId);
     }
 
+    /**
+     * Retrieves the starting objectives from the server.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void getStartingObjectives() throws  ServerConnectionException, RemoteException{
         Server.getStartingObjectives(this.gameId, data.getUsername());
     }
 
     //TODO: methods to implement
+
+    /**
+     * Posts a chat message on the server.
+     * @param message The chat message to post.
+     * @throws ServerConnectionException If there is an issue connecting to the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void postChat(String message) throws  ServerConnectionException, RemoteException{
         server.postChat(this.gameId, data.getUsername(), message);
     }
 
+    /**
+     * Waits for an update from the server.
+     * @throws RemoteException If a remote communication error occurs.
+     */
     public void waitUpdate() throws RemoteException {
         server.waitUpdate(this.gameId, data.getUsername());
     }
