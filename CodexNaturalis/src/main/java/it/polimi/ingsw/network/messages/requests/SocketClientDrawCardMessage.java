e40dbafe3cc25db983f6e8459834e22b938b2b66
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-04-30 14:46:28 +0200

    docs: added javadoc to SocketClientDrawCardMessage

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientDrawCardMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientDrawCardMessage.java
@@ -2,9 +2,22 @@ package it.polimi.ingsw.network.messages.requests;
 
 import java.util.UUID;
 
+/**
+ * Represents a message from a client to request drawing a card in a game via socket network communication.
+ * This class extends the `GenericRequestMessage` class and contains the username of the client making the request,
+ * the position of the card to be drawn, and the UUID of the game.
+ */
 public class SocketClientDrawCardMessage extends GenericRequestMessage {
     private final int position;
     private final UUID gameUUID;
+
+    /**
+     * Constructs a new SocketClientDrawCardMessage with the specified username, position, and game UUID.
+     *
+     * @param username The username of the client making the request.
+     * @param position The position of the card to be drawn (e.g., index in the deck).
+     * @param gameUUID The UUID of the game in which the card will be drawn.
+     */
 
     public SocketClientDrawCardMessage(String username, int position, UUID gameUUID) {
         this.username = username;
@@ -12,10 +25,18 @@ public class SocketClientDrawCardMessage extends GenericRequestMessage {
         this.gameUUID = gameUUID;
     }
 
+    /**
+     * Gets the position of the card to be drawn.
+     * @return The position of the card.
+     */
     public int getPosition() {
         return position;
     }
 
+    /**
+     * Gets the UUID of the game in which the card will be drawn.
+     * @return The UUID of the game.
+     */
     public UUID getGameUUID() {
         return gameUUID;
     }
