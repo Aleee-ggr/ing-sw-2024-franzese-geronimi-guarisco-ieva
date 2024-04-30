e51277f44a75163f3f5ac46a4c52ce935738aea5
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-04-30 14:50:48 +0200

    docs: added javadoc to SocketClientGetCommonObjectivesMessage

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientGetCommonObjectivesMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientGetCommonObjectivesMessage.java
@@ -2,14 +2,29 @@ package it.polimi.ingsw.network.messages.requests;
 
 import java.util.UUID;
 
+/**
+ * Represents a message from a client to request information about the common objectives in a game
+ * via socket network communication.
+ * This class extends the GenericRequestMessage class and contains the username of the client making the request,
+ * and the UUID of the game.
+ */
 public class SocketClientGetCommonObjectivesMessage extends GenericRequestMessage {
     private final UUID gameUUID;
 
+    /**
+     * Constructs a new SocketClientGetCommonObjectivesMessage with the specified username and game UUID.
+     * @param username The username of the client making the request.
+     * @param gameUUID The UUID of the game for which to retrieve the common objectives.
+     */
     public SocketClientGetCommonObjectivesMessage(String username, UUID gameUUID) {
         this.username = username;
         this.gameUUID = gameUUID;
     }
 
+    /**
+     * Gets the UUID of the game for which to retrieve the common objectives.
+     * @return The UUID of the game.
+     */
     public UUID getGameUUID() {
         return gameUUID;
     }
