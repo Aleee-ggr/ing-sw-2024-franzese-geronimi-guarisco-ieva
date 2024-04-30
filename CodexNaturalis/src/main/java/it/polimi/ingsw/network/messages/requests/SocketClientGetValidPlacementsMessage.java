5d2772c3de0ff91265b40a3d95f38566a25c4a30
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-04-30 15:04:33 +0200

    docs: added javadoc to SocketClientGetValidPlacementsMessage

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientGetValidPlacementsMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientGetValidPlacementsMessage.java
@@ -2,14 +2,29 @@ package it.polimi.ingsw.network.messages.requests;
 
 import java.util.UUID;
 
+/**
+ * Represents a message from a client to request information about valid placements in a game
+ * via socket network communication.
+ * This class extends the GenericRequestMessage class and contains the username of the client making the request
+ * and the UUID of the game.
+ */
 public class SocketClientGetValidPlacementsMessage extends GenericRequestMessage {
     private final UUID gameUUID;
 
+    /**
+     * Constructs a new SocketClientGetValidPlacementsMessage with the specified username and game UUID.
+     * @param username The username of the client making the request.
+     * @param gameUUID The UUID of the game from which to retrieve the valid placements.
+     */
     public SocketClientGetValidPlacementsMessage(String username, UUID gameUUID){
         this.username = username;
         this.gameUUID = gameUUID;
     }
 
+    /**
+     * Gets the UUID of the game from which to retrieve the valid placements.
+     * @return The UUID of the game.
+     */
     public UUID getGameUUID() {
         return gameUUID;
     }
