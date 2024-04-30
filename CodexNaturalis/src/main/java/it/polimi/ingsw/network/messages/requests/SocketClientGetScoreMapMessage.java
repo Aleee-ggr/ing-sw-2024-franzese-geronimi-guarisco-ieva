738bfb04489dcbe518703757f085bb0ab29f5e8b
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-04-30 15:00:58 +0200

    docs: added javadoc to SocketClientGetScoreMapMessage

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientGetScoreMapMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientGetScoreMapMessage.java
@@ -1,7 +1,16 @@
 package it.polimi.ingsw.network.messages.requests;
 
+/**
+ * Represents a message from a client to request information about the score map in a game
+ * via socket network communication.
+ * This class extends the GenericRequestMessage class and contains the username of the client making the request.
+ */
 public class SocketClientGetScoreMapMessage extends GenericRequestMessage {
 
+    /**
+     * Constructs a new SocketClientGetScoreMapMessage with the specified username.
+     * @param username The username of the client making the request.
+     */
     public SocketClientGetScoreMapMessage(String username){
         this.username = username;
     }
