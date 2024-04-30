204424e1d6f01e3d224b0d52ed4c8afb043bb227
Daniele Ieva <daniele.ieva@hotmail.com>
2024-04-30 13:38:16 +0200

    feat: added resource view

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/components/ResourceView.java
@@ -0,0 +1,53 @@
+package it.polimi.ingsw.view.TUI.components;
+
+import it.polimi.ingsw.model.enums.Resource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Math.min;
+
+public class ResourceView {
+    Map<Resource, Integer> resourceCount;
+
+    public ResourceView() {
+        resourceCount = new HashMap<>();
+        for (Resource res : Resource.values()) {
+            if (res == Resource.NONE || res == Resource.NONCOVERABLE) {
+                continue;
+            }
+            resourceCount.put(res, 0);
+        }
+    }
+
+    public void setResourceCount(Map<Resource, Integer> resourceCount) {
+        this.resourceCount =new HashMap<>(resourceCount);
+    }
+
+    public void setResource(Resource resource, Integer count) {
+        this.resourceCount.put(resource, count);
+    }
+
+    public String[] toStringArray() {
+        return toString().split("\n");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder out = new StringBuilder();
+        for (Resource res : Resource.values()) {
+            if (res == Resource.NONE || res == Resource.NONCOVERABLE ) {
+                continue;
+            }
+
+            out.append(" ")
+                    .append(res.toChar())
+                    .append(" ")
+                    .append("੦".repeat(min(65, resourceCount.get(res))))
+                    .append(" ".repeat(min(1, resourceCount.get(res))))
+                    .append(resourceCount.get(res))
+                    .append("\n");
+        }
+        return out.toString();
+    }
+}
---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/view/TUI/ResourceViewTest.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.view.TUI;
+
+import it.polimi.ingsw.model.enums.Resource;
+import it.polimi.ingsw.view.TUI.components.ResourceView;
+import org.junit.Test;
+
+public class ResourceViewTest {
+    @Test
+    public void noResource() {
+        ResourceView view = new ResourceView();
+        System.out.println(view);
+    }
+
+    @Test
+    public void F10P15() {
+        ResourceView view = new ResourceView();
+        view.setResource(Resource.FUNGI, 10);
+        view.setResource(Resource.PLANT, 15);
+        System.out.println(view);
+    }
+
+    @Test
+    public void Overflow() {
+        ResourceView view = new ResourceView();
+        view.setResource(Resource.FUNGI, 64);
+        view.setResource(Resource.PLANT, 999);
+        System.out.println(view);
+    }
+}
