a1a8050994689a18255dbd493bf3b8fbca960c1f
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-05-04 18:29:04 +0200

    test: started SocketTest class

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/network/SocketTest.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.network;
+
+import it.polimi.ingsw.network.socket.SocketClient;
+import it.polimi.ingsw.network.socket.SocketServer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class SocketTest {
+    private static SocketServer server;
+
+    @BeforeClass
+    public static void setup() {
+        server = new SocketServer(9090);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        server.stopServer();
+    }
+
+    @Test
+    public void connectionTest() {
+        SocketClient client = new SocketClient("prova", "pippo", "localhost", 9090);
+        client.startConnection("localhost", 9090);
+
+        try {
+            client.stopConnection();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
