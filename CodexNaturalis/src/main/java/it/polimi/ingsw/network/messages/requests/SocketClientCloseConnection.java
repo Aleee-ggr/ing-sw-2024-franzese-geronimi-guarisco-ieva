48914b7d049da9e29ad6fa2d7842df8fc383e011
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-05-06 19:48:20 +0200

    docs: added Javadoc to new request messages

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientCloseConnection.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientCloseConnection.java
@@ -1,6 +1,14 @@
 package it.polimi.ingsw.network.messages.requests;
 
+/**
+ * Represents a request message sent by a client to close the connection.
+ */
 public class SocketClientCloseConnection extends GenericRequestMessage{
+
+    /**
+     * Constructs a new SocketClientCloseConnection request message.
+     * @param username the username of the client sending the request to close the connection.
+     */
     public SocketClientCloseConnection(String username){
         this.username = username;
     }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientGetStartingCardMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/requests/SocketClientGetStartingCardMessage.java
@@ -2,14 +2,26 @@ package it.polimi.ingsw.network.messages.requests;
 
 import java.util.UUID;
 
+/**
+ * Represents a request message sent by a client to retrieve the starting card in a game.
+ */
 public class SocketClientGetStartingCardMessage extends GenericRequestMessage{
     private final UUID gameUUID;
 
+    /**
+     * Constructs a new SocketClientGetStartingCardMessage.
+     * @param username the username of the client sending the request.
+     * @param gameUUID the unique identifier of the game for which the starting card is requested.
+     */
     public SocketClientGetStartingCardMessage(String username, UUID gameUUID){
         this.username = username;
         this.gameUUID = gameUUID;
     }
 
+    /**
+     * Gets the unique identifier of the game for which the starting card is requested.
+     * @return the UUID of the game.
+     */
     public UUID getGameUUID() {
         return gameUUID;
     }
