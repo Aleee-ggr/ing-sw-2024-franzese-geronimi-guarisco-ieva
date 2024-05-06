22598171d4c38130b56d4c040bf836143fba6657
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-05-06 14:17:22 +0200

    feat: add ClientApp with main function

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/ClientApp.java
@@ -0,0 +1,53 @@
+package it.polimi.ingsw.network;
+
+import it.polimi.ingsw.network.rmi.RmiClient;
+import it.polimi.ingsw.network.socket.SocketClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ClientApp {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to CODEX NATURALIS!");
+        System.out.println("Please, insert your username and password to register.");
+        System.out.println("Username: ");
+        String username = br.readLine();
+        System.out.println("Password: ");
+        String password = br.readLine();
+        System.out.println("Do you have any preferences for technology?(1 -> RMI,  else -> SOCKET)");
+        int tech = 0;
+        try {
+            tech = Integer.parseInt(br.readLine());
+        }catch (NumberFormatException e){
+            System.out.println("Invalid input, using SOCKET as default.");
+        }
+        System.out.println("Please, insert the server IP address and port.");
+        System.out.println("IP address: ");
+        String ip = br.readLine();
+        System.out.println("Port: (default 9090)");
+        int port = 9090;
+        try {
+            port = Integer.parseInt(br.readLine());
+        }catch (NumberFormatException e){
+            System.out.println("Invalid input, using 9090 as default port.");
+        }
+
+        if (tech == 1) {
+            try {
+                RmiClient client = new RmiClient(username, password, ip, port);
+            } catch (Exception e) {
+                System.out.println("Error with the connection:" + e.getMessage());
+            }
+        } else {
+            try {
+                SocketClient client = new SocketClient(username, password, ip, port);
+                client.startConnection(ip, port);
+            } catch (Exception e) {
+                System.out.println("Error with the connection:" + e.getMessage());
+            }
+        }
+    }
+
+}
