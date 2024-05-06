553fa1d93772753db15740a3b09c168a8472ab51
Daniele Ieva <daniele.ieva@hotmail.com>
2024-05-06 22:37:23 +0200

    feat: improved starting configuration

--- CodexNaturalis/src/main/java/it/polimi/ingsw/App.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/App.java
@@ -3,11 +3,12 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.helpers.exceptions.network.ServerConnectionException;
 import it.polimi.ingsw.network.rmi.RmiClient;
 import it.polimi.ingsw.network.rmi.RmiServer;
+import it.polimi.ingsw.view.TUI.controller.TuiController;
 
 import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Hello world!
@@ -18,7 +19,7 @@ public class App
     public static void main( String[] args) throws RemoteException, ServerConnectionException {
         List<String> arguments = List.of(args);
         if (arguments.contains("server")) {
-            new RmiServer(1234);
+            new RmiServer(9090);
         }
         if (arguments.contains("client")) {
             System.out.print("insert connection mode\n1) socket\n2) RMI\n");
@@ -28,10 +29,12 @@ public class App
                 case 1:
                     break;
                 case 2:
-                    RmiClient client = new RmiClient("username", "password","localhost", 1234);
-                    UUID game = client.newGame(3);
-                    client.setGameId(game);
-                    client.joinGame(game);
+                    RmiClient client = new RmiClient(
+                            String.valueOf(ThreadLocalRandom.current().nextInt(100, 10000)),
+                            "password",
+                            "localhost",
+                            9090);
+                    new TuiController(client, null);
                     break;
                 default:
                     System.out.println("Unknown option!");
