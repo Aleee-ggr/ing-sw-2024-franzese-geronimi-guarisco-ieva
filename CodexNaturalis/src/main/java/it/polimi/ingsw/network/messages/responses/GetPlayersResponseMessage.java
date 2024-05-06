0ba14615e988f5a0673f2af9822e8174e490cbfc
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-05-06 19:48:35 +0200

    docs: added Javadoc to new response messages

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/responses/GetPlayersResponseMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/responses/GetPlayersResponseMessage.java
@@ -2,13 +2,25 @@ package it.polimi.ingsw.network.messages.responses;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a response message containing a list of players in a game.
+ * This message is sent in response to a request for the list of players in a game.
+ */
 public class GetPlayersResponseMessage extends GenericResponseMessage{
     private final ArrayList<String> players;
 
+    /**
+     * Constructs a new GetPlayersResponseMessage with the specified list of players.
+     * @param players the list of players in the game.
+     */
     public GetPlayersResponseMessage(ArrayList<String> players) {
         this.players = players;
     }
 
+    /**
+     * Returns the list of players in the game.
+     * @return the list of players.
+     */
     public ArrayList<String> getPlayers() {
         return players;
     }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/responses/GetStartingCardResponseMessage.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/messages/responses/GetStartingCardResponseMessage.java
@@ -1,12 +1,24 @@
 package it.polimi.ingsw.network.messages.responses;
 
+/**
+ * Represents a response message that contains the ID of the starting card in a game.
+ * This message is sent in response to a request for the starting card ID.
+ */
 public class GetStartingCardResponseMessage extends GenericResponseMessage{
     private final Integer startingCardId;
 
+    /**
+     * Constructs a new GetStartingCardResponseMessage with the specified starting card ID.
+     * @param startingCardId the ID of the starting card in the game.
+     */
     public GetStartingCardResponseMessage(Integer startingCardId) {
         this.startingCardId = startingCardId;
     }
 
+    /**
+     * Returns the ID of the starting card in the game.
+     * @return the starting card ID.
+     */
     public Integer getStartingCardId() {
         return startingCardId;
     }
