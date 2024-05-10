2c830677c52f70a85e166d0da3878097f2a82e06
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-05-10 19:05:01 +0200

    feat: created GameIntroductionController

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/GameIntroductionController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/GameIntroductionController.java
@@ -1,4 +1,34 @@
 package it.polimi.ingsw.view.GUI.SceneControllers;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class GameIntroductionController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void changeConnectionScene(ActionEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/GameIntroduction.fxml"));
+            Scene scene = new Scene(loader.load(), 1600, 900);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void exit(ActionEvent event) {
+        Platform.exit();
+    }
 }
