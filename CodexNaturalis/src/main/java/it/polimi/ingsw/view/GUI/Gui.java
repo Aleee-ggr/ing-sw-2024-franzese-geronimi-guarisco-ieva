e47e49dfd2ae13db7ed56c38b63074715235859b
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-05-10 19:04:23 +0200

    feat: created javafx main class

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Gui.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.view.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Gui extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource("/GUI/fxml/GameIntroduction.fxml"));
+        System.out.println(fxmlLoader.getLocation());
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
