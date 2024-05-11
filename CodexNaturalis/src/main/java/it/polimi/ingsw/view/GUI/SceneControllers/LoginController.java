f4b9f72b53ef176c3228ced4fe49c5f953984ea3
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-05-11 10:28:09 +0200

    feat: created javafx controllers

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/LoginController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/LoginController.java
@@ -1,4 +1,37 @@
 package it.polimi.ingsw.view.GUI.SceneControllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    @FXML
+    protected void changeMainMenuScene(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/fxml/GameIntroduction.fxml"));
+            Scene scene = new Scene(loader.load(), 1600, 900);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void goBack(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/fxml/ConnectionScene.fxml"));
+            Scene scene = new Scene(loader.load(), 1600, 900);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ServerConnectionController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ServerConnectionController.java
@@ -1,4 +1,37 @@
 package it.polimi.ingsw.view.GUI.SceneControllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ServerConnectionController {
+
+    @FXML
+    protected void changeLoginScene(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/fxml/LoginScene.fxml"));
+            Scene scene = new Scene(loader.load(), 1600, 900);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void goBack(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/fxml/GameIntroduction.fxml"));
+            Scene scene = new Scene(loader.load(), 1600, 900);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
