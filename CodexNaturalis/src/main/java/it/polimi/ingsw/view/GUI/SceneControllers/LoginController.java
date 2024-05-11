a30c7ff332ca124e1c7ed334996ab9e9cacf4dd0
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-05-11 19:03:21 +0200

    feat: added setClient method and setClient into next controller

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/LoginController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/LoginController.java
@@ -1,20 +1,33 @@
 package it.polimi.ingsw.view.GUI.SceneControllers;
 
+import it.polimi.ingsw.network.ClientInterface;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
+    private ClientInterface client;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
 
     @FXML
     protected void changeMainMenuScene(ActionEvent event) {
+        client.setCredentials(usernameField.getText(), passwordField.getText());
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/fxml/GameIntroduction.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/fxml/MainMenu.fxml"));
+            MainMenuController controller = new MainMenuController();
+            controller.setClient(client);
+            loader.setController(controller);
             Scene scene = new Scene(loader.load(), 1600, 900);
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(scene);
@@ -33,5 +46,9 @@ public class LoginController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    protected void setClient(ClientInterface client) {
+        this.client = client;
     }
 }
