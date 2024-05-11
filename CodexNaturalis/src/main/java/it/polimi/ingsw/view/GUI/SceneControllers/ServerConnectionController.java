aca6f3fdc8e4a2f039329a1e3f5dbd04230f1ae8
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-05-11 19:01:09 +0200

    feat: added client creation into changeLoginScene

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ServerConnectionController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ServerConnectionController.java
@@ -1,21 +1,33 @@
 package it.polimi.ingsw.view.GUI.SceneControllers;
 
+import it.polimi.ingsw.network.ClientInterface;
+import it.polimi.ingsw.network.rmi.RmiClient;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ServerConnectionController {
+    @FXML
+    TextField serverIp;
+
+    @FXML
+    TextField serverPort;
 
     @FXML
     protected void changeLoginScene(ActionEvent event) {
+        ClientInterface client = new RmiClient(serverIp.getText(), Integer.parseInt(serverPort.getText()));
+
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/fxml/LoginScene.fxml"));
             Scene scene = new Scene(loader.load(), 1600, 900);
+            LoginController loginController = loader.getController();
+            loginController.setClient(client);
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(scene);
         } catch (IOException e) {
