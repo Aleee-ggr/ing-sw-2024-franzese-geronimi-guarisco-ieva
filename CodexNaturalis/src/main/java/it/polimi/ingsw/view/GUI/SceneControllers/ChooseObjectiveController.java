eb19dad5140f4255ade497eef867ceaa5b78b3ff
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-05-14 13:55:15 +0200

    feat: added objectives images

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ChooseObjectiveController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ChooseObjectiveController.java
@@ -17,6 +17,9 @@ public class ChooseObjectiveController implements Initializable {
     @FXML
     private ImageView firstObjective;
 
+    @FXML
+    private ImageView secondObjective;
+
     public void setClient(ClientInterface client) {
         this.client = client;
     }
@@ -25,11 +28,16 @@ public class ChooseObjectiveController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         PlayerData playerData = client.getPlayerData();
         int firstObjectiveId = playerData.getStartingObjectives().getFirst().getId();
-        String imagePath = "008.png";
+        String imagePath = String.format("GUI/images/cards/%03d.png", firstObjectiveId);
         System.out.println(imagePath);
 
         Image image = new Image(imagePath);
         firstObjective.setImage(image);
+
+        int secondObjectiveId = playerData.getStartingObjectives().getLast().getId();
+        String imagePathSecond = String.format("GUI/images/cards/%03d.png", secondObjectiveId);
+        Image imageSecond = new Image(imagePathSecond);
+        secondObjective.setImage(imageSecond);
     }
 
     public void changeChooseStarting(MouseEvent mouseEvent) {
