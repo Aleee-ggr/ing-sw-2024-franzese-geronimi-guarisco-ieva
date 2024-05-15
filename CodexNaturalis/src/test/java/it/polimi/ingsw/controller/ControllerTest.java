5d109cd386ec8ef43ef972c6b0821d218fede944
Daniele Ieva <daniele.ieva@hotmail.com>
2024-05-15 09:17:18 +0200

    test: created test for validPosition

--- CodexNaturalis/src/test/java/it/polimi/ingsw/controller/ControllerTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/controller/ControllerTest.java
@@ -3,15 +3,18 @@ package it.polimi.ingsw.controller;
 import it.polimi.ingsw.GameConsts;
 import it.polimi.ingsw.controller.threads.Status;
 import it.polimi.ingsw.controller.threads.ThreadMessage;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.board.Coordinates;
+import it.polimi.ingsw.model.cards.Corner;
+import it.polimi.ingsw.model.cards.StartingCard;
+import it.polimi.ingsw.model.enums.Resource;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
 
 import static java.lang.Math.abs;
 import static org.junit.Assert.*;
@@ -237,6 +240,48 @@ public class ControllerTest {
         }
     }
 
+    @Test
+    public void testValidPlacements() throws InterruptedException {
+        String[] usernames = {"p1", "p2"};
+        fillGame(usernames);
+        UUID msgUUID = UUID.randomUUID();
+
+        controller.getStartingCards(usernames[0], msgUUID);
+        int id = Integer.parseInt(msgQueue.take().args()[0]);
+
+        StartingCard card = (StartingCard) Game.getCardByID(id);
+
+        controller.placeStartingCard(usernames[0], true, msgUUID);
+        msgQueue.take();
+
+        controller.getValidPlacements(usernames[0], msgUUID);
+        ThreadMessage msg = msgQueue.take();
+
+        Set<Coordinates> validCoordinates = new HashSet<>(4);
+        List<Resource> res = Arrays.stream(card.getFrontCorners())
+                .map(Corner::getCornerResource)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        if (res.removeFirst() != Resource.NONCOVERABLE) {
+            validCoordinates.add(new Coordinates(0, 1));
+        }
+        if (res.removeFirst() != Resource.NONCOVERABLE) {
+            validCoordinates.add(new Coordinates(-1, 0));
+        }
+        if (res.removeFirst() != Resource.NONCOVERABLE) {
+            validCoordinates.add(new Coordinates(1, 0));
+        }
+        if (res.removeFirst() != Resource.NONCOVERABLE) {
+            validCoordinates.add(new Coordinates(0, -1));
+        }
+
+        for (String data : msg.args()) {
+            String[] split = data.split(",");
+            int x = Integer.parseInt(split[0]);
+            int y = Integer.parseInt(split[1]);
+            assertTrue(validCoordinates.contains(new Coordinates(x, y)));
+        }
+    }
     private void fillGame(String[] usernames) throws InterruptedException {
         for (String username : usernames) {
             UUID msgUUID = UUID.randomUUID();
