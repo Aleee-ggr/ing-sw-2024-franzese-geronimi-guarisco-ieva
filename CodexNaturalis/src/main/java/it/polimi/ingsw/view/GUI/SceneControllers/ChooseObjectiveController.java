28f4302e5ba0c4b02dc5e23649f3176b7edd59a6
gloriageronimi <161638688+gloriageronimi@users.noreply.github.com>
2024-05-15 09:05:06 +0200

    fix: fixed images path

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ChooseObjectiveController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ChooseObjectiveController.java
@@ -28,14 +28,14 @@ public class ChooseObjectiveController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         PlayerData playerData = client.getPlayerData();
         int firstObjectiveId = playerData.getStartingObjectives().getFirst().getId();
-        String imagePath = String.format("GUI/images/cards/%03d.png", firstObjectiveId);
+        String imagePath = String.format("GUI/images/cards/front/%03d.png", firstObjectiveId);
         System.out.println(imagePath);
 
         Image image = new Image(imagePath);
         firstObjective.setImage(image);
 
         int secondObjectiveId = playerData.getStartingObjectives().getLast().getId();
-        String imagePathSecond = String.format("GUI/images/cards/%03d.png", secondObjectiveId);
+        String imagePathSecond = String.format("GUI/images/cards/front/%03d.png", secondObjectiveId);
         Image imageSecond = new Image(imagePathSecond);
         secondObjective.setImage(imageSecond);
     }
