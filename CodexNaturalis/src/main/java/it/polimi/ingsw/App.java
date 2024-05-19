404ff902484b40aa36a8f167c8b2c46bfc268441
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-05-19 16:26:12 +0200

    feat: players can now choose to use socket

--- CodexNaturalis/src/main/java/it/polimi/ingsw/App.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/App.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.helpers.exceptions.network.ServerConnectionException;
 import it.polimi.ingsw.network.rmi.RmiClient;
 import it.polimi.ingsw.network.rmi.RmiServer;
+import it.polimi.ingsw.network.socket.SocketClient;
+import it.polimi.ingsw.network.socket.SocketServer;
 import it.polimi.ingsw.view.TUI.controller.TuiController;
 
 import java.rmi.RemoteException;
@@ -21,26 +23,35 @@ public class App
         List<String> arguments = List.of(args);
         if (arguments.contains("server")) {
             new RmiServer(9090);
+            new SocketServer(6969);
         }
         if (arguments.contains("client")) {
             System.out.print("insert connection mode\n1) socket\n2) RMI\n");
             String input = new Scanner(System.in).next();
             int mode = Integer.parseInt(input);
+
+
+            System.out.print("Insert server address: ");
+            input = new Scanner(System.in).next();
+            String serverAddress = "localhost";
+            if (input.matches("([0-9]+\\.)+[0-9]+")) {
+                serverAddress = input;
+            }
+
             switch (mode) {
                 case 1:
+                    System.setProperty("java.socket.server.hostname", serverAddress);
+                    SocketClient clientSocket = new SocketClient(
+                            serverAddress,
+                            6969);
+                    new TuiController(clientSocket).start();
                     break;
                 case 2:
-                    System.out.print("Insert server address: ");
-                    input = new Scanner(System.in).next();
-                    String serverAddress = "localhost";
-                    if (input.matches("([0-9]+\\.)+[0-9]+")) {
-                        serverAddress = input;
-                    }
                     System.setProperty("java.rmi.server.hostname", serverAddress);
-                    RmiClient client = new RmiClient(
+                    RmiClient clientRmi = new RmiClient(
                             serverAddress,
                             9090);
-                    new TuiController(client).start();
+                    new TuiController(clientRmi).start();
                     break;
                 default:
                     System.out.println("Unknown option!");
