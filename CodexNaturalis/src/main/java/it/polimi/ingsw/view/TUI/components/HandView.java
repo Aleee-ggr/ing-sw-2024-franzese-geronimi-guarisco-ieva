98b7f864ea384982cc163b42f068e6f1d363a43a
Daniele Ieva <daniele.ieva@hotmail.com>
2024-05-29 14:04:50 +0200

    fix: fix after merge

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/components/HandView.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/TUI/components/HandView.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.view.TUI.components;
 import it.polimi.ingsw.GameConsts;
 import it.polimi.ingsw.model.cards.Card;
 import it.polimi.ingsw.model.cards.GoldCard;
+import it.polimi.ingsw.model.client.OpponentData;
 import it.polimi.ingsw.model.enums.Resource;
 import it.polimi.ingsw.network.ClientInterface;
 import it.polimi.ingsw.view.TUI.components.printables.CardBack;
@@ -18,53 +19,96 @@ public class HandView implements Component{
 
     private final ClientInterface client;
 
+    private String currentPlayer;
     @Override
     public String toString()  {
         return setHand();
     }
 
+    public void setCurrentPlayer(String currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
     public HandView(ClientInterface client) {
         this.client = client;
+        currentPlayer = client.getUsername();
     }
 
     private String setHand() {
-        List<Card> cardsToPrint = client.getPlayerData().getClientHand();
         StringBuilder out = new StringBuilder();
         int leftRightPadding = (panelWidth - PrintCards.width) / 2;
         Integer cardNum = 1;
 
-        for (Card card : cardsToPrint) {
-            out.append(cardNum)
-                    .append(".");
-            boolean firstLine = true;
+        if (currentPlayer.equals(client.getUsername())) {
+            List<Card> cardsToPrint = client.getPlayerData().getClientHand();
 
-            for (String line : new PrintCards(card).toString().split("\n")) {
+            for (Card card : cardsToPrint) {
+                out.append(cardNum)
+                        .append(".");
+                boolean firstLine = true;
 
-                if(firstLine){
-                    out.append(line)
-                        .append(" ".repeat(leftRightPadding))
+                for (String line : new PrintCards(card).toString().split("\n")) {
+
+                    if (firstLine) {
+                        out.append(line)
+                                .append(" ".repeat(leftRightPadding))
+                                .append("\n");
+                        firstLine = false;
+                    } else {
+                        out.append(" ".repeat(leftRightPadding))
+                                .append(line)
+                                .append(" ".repeat(leftRightPadding))
+                                .append("\n");
+                    }
+                }
+
+                cardNum++;
+            }
+
+            for (int i = 0; i < cardsToPrint.size() - GameConsts.firstHandDim; i++) {
+                for (int j = 0; j < PrintCards.height; j++) {
+                    out.append(" ".repeat(panelWidth))
+                            .append("\n");
+                }
+            }
+            for (int i = 0; i < panelHeight - cardsToPrint.size() * (PrintCards.height); i++) {
+                out.append(" ".repeat(panelWidth))
                         .append("\n");
-                    firstLine = false;
-                } else {
+            }
+        }
+
+
+        else {
+            OpponentData data = (OpponentData) client.getOpponentData().get(currentPlayer);
+            List<Resource> cardsToPrint = data.getHandColor();
+            for (Resource r : cardsToPrint) {
+                out.append(cardNum)
+                        .append(".");
+                out.append(" ".repeat(panelWidth - (Integer.toString(cardNum).length()) - 1));
+                out.append("\n");
+                for (String line : CardBack.resources.get(r).split("\n")) {
                     out.append(" ".repeat(leftRightPadding))
                             .append(line)
                             .append(" ".repeat(leftRightPadding))
                             .append("\n");
                 }
+                out.append(" ".repeat(panelWidth));
+                out.append("\n");
+                cardNum++;
             }
 
-            cardNum++;
-        }
-
-        for (int i = 0; i < cardsToPrint.size() - GameConsts.firstHandDim; i++) {
-            for (int j = 0; j < PrintCards.height; j++) {
+            for (int i = 0; i < cardsToPrint.size() - GameConsts.firstHandDim; i++) {
+                for (int j = 0; j < CardBack.height + 2; j++) {
+                    out.append(" ".repeat(panelWidth))
+                            .append("\n");
+                }
+            }
+            for (int i = 0; i < panelHeight - cardsToPrint.size() * (PrintCards.height + 2); i++) {
                 out.append(" ".repeat(panelWidth))
                         .append("\n");
             }
-        }
-        for (int i = 0; i < panelHeight - cardsToPrint.size() * (PrintCards.height); i++) {
             out.append(" ".repeat(panelWidth))
-                    .append("\n");
+                            .append("\n");
         }
         return out.toString();
     }
