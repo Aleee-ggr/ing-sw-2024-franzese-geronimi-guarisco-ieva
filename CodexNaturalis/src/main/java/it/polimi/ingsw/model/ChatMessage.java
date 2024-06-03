5bc1f53e0730bb6596ed6e4ab0a45bc2a2b6de75
Daniele Ieva <daniele.ieva@hotmail.com>
2024-06-03 10:31:39 +0200

    feat: created chatMessage class and filters

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ChatMessage.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.model;
+
+import java.util.function.Predicate;
+
+public record ChatMessage(String sender, String receiver, String message) {
+
+    public boolean filterPlayer(String player) {
+        return sender.equals(player) || receiver == null || receiver.equals(player);
+    }
+
+    public static Predicate<ChatMessage> getPlayerFilter(String player) {
+        return (ChatMessage message) -> message.filterPlayer(player);
+    }
+
+    @Override
+    public String toString() {
+        return sender + ": " + message;
+    }
+}
