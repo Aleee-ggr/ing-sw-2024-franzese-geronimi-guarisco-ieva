2066144f80a956de2a4ef5b633faa41d9805660b
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-06-06 19:20:32 +0200

    style: SocketServer removed warnings and fix docs

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/SocketServer.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/SocketServer.java
@@ -9,12 +9,18 @@ import java.net.Socket;
 /**
  * The SocketServer class extends the Server class and represents a server that uses sockets
  * for communication with clients.
+ *
  * @author Samuele Franzese
  */
 public class SocketServer extends Server {
     private ServerSocket server;
     private Socket socket;
 
+    /**
+     * Constructs a new SocketServer and starts listening for client connections on the specified port.
+     *
+     * @param port The port on which the server will listen for client connections.
+     */
     public SocketServer(int port) {
         System.out.println("Starting server...");
         try {
@@ -23,29 +29,21 @@ public class SocketServer extends Server {
             new Thread(this::acceptConnection).start();
         } catch (IOException e) {
             e.printStackTrace();
+            System.exit(1);
         }
-    }
-
-    /**
-     * Starts the server on the specified port.
-     *
-     * @param port the port number on which the server will listen for incoming connections
-     * @throws IOException if an I/O error occurs while creating the server socket
-     */
-    public void startServer(int port) throws IOException{
-
     }
 
     /**
      * Accepts client connections and creates a new ClientHandler for each accepted connection.
      */
     public void acceptConnection() {
-        while(!server.isClosed()) {
+        while (!server.isClosed()) {
             try {
                 socket = server.accept();
                 new ClientHandler(socket).start();
             } catch (IOException e) {
                 e.printStackTrace();
+                System.exit(1);
             }
         }
     }
