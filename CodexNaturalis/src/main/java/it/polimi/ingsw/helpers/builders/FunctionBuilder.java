69d2bb476fc1e37d830e35a09702d420ba691328
Daniele Ieva <daniele.ieva@hotmail.com>
2024-06-14 12:20:11 +0200

    fix: changed path to string

--- CodexNaturalis/src/main/java/it/polimi/ingsw/helpers/builders/FunctionBuilder.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/helpers/builders/FunctionBuilder.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.helpers.builders;
 
 import it.polimi.ingsw.helpers.exceptions.model.InvalidTypeException;
+import it.polimi.ingsw.helpers.pattern.PatternMatcher;
 import it.polimi.ingsw.model.board.Coordinates;
 import it.polimi.ingsw.model.board.PlayerBoard;
 import it.polimi.ingsw.model.enums.Resource;
-import it.polimi.ingsw.helpers.pattern.PatternMatcher;
 import it.polimi.ingsw.model.player.Player;
 
 import java.util.*;
@@ -13,19 +13,12 @@ import java.util.function.Function;
 import static java.lang.Math.signum;
 
 public class FunctionBuilder {
-    public static final String[] validTypes = {
-            "none",
-            "pattern",
-            "resource",
-            "resources",
-            "cover",
-    };
+    public static final String[] validTypes = { "none", "pattern", "resource", "resources", "cover", };
+    private final Map<Coordinates, Resource> pattern = new HashMap<>(3);
     private int points;
     private String type;
-    private  Map<Resource, Integer> resources;
-    private final Map<Coordinates, Resource> pattern = new HashMap<>(3);
+    private Map<Resource, Integer> resources;
     private Resource resource;
-
 
     /**
      * @return a list of valid types for the method {@link #setType(String) setType}
@@ -35,137 +28,8 @@ public class FunctionBuilder {
     }
 
     /**
-     * Set the resources and their amount to use to calculate the score  when paired with the type "resources"
-     * @see #setType(String)
-     * @see #setPoints(int)
-     * @see #build()
-     * @param resources the resource the function will use to calculate the score
-     * @return this object
-     */
-    public FunctionBuilder setResources(Map<Resource, Integer> resources) {
-        this.resources = resources;
-        return this;
-    }
-
-    /**
-     * A multiplier of the points a certain type yields
-     * @param points the multiplier the function will use to calculate the score
-     * @return this object
-     */
-    public FunctionBuilder setPoints(int points) {
-        this.points = points;
-        return this;
-    }
-
-    /**
-     * A pattern that will yield points if it is matched on the game grid when the type is "pattern"
-     * @see #setType(String)  
-     * @see #setPoints(int)
-     * @see #build()
-     * @param pattern the pattern to use
-     * @return this object
-     */
-    public FunctionBuilder setPattern(Resource[][] pattern) {
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
-                if (pattern[y][x] != Resource.NONE) {
-                    this.pattern.put(
-                            rotate(new Coordinates(x - 1, y - 1)),
-                            pattern[y][x]
-                    );
-                }
-            }
-        }
-        return this;
-    }
-
-    /**
-     * Set the type of the function, listed in {@link #getValidTypes()},
-     * to choose the strategy used to calculate the player score.
-     * @param type the type of the function calculation
-     * @return this object
-     * @throws InvalidTypeException when the type is not included in {@link #getValidTypes()}
-     */
-    public FunctionBuilder setType(String type) throws InvalidTypeException {
-        if (!Arrays.asList(validTypes).contains(type)) {
-            throw new InvalidTypeException(type + " is not a valid type");
-        }
-        this.type = type;
-        return this;
-    }
-
-    /**
-     * Set the resource to use to calculate the score  when paired with the type "resource"
-     * @see #setType(String)
-     * @see #setPoints(int)
-     * @see #build()
-     * @param resource the resource the function will use to calculate the score
-     * @return this object
-     */
-    public FunctionBuilder setResource(Resource resource) {
-        this.resource = resource;
-        return this;
-    }
-
-    /**
-     * returns a function of the given type to calculate a player score, using the given type and score
-     * @see #setType(String)
-     * @see Player
-     * @return a function of the given type to calculate a player score
-     * @throws InvalidTypeException when the type is not included in {@link #getValidTypes()}
-     */
-    public Function<Player, Integer> build() throws InvalidTypeException {
-        return switch (type) {
-            case "none" ->
-                    (Player player) -> points;
-
-            case "pattern" ->
-                    (Player player) -> {
-                        PlayerBoard board = player.getPlayerBoard();
-                        int matches_found = new PatternMatcher()
-                                .setPattern(pattern)
-                                .setBoard(board)
-                                .matches_found();
-                        return matches_found * points;
-                    };
-
-            case "cover" ->
-                    (Player player) -> {
-                        PlayerBoard board = player.getPlayerBoard();
-                        Coordinates lastCoordinates = board.getLastPlacedPosition();
-                        int neighbor_count = 0;
-                        for (Coordinates neighbor : lastCoordinates.getNeighbors()) {
-                            if (board.getCard(neighbor) != null) {
-                                neighbor_count++;
-                            }
-                        }
-                        return neighbor_count * points;
-                    };
-
-            case "resources" ->
-                    (Player player) -> {
-                        Map<Resource, Integer> playerResources = player.getResources();
-                        int resourcePoints;
-                        Map<Resource, Integer> result = new HashMap<>();
-                        for (Resource res : resources.keySet()) {
-                            resourcePoints = (playerResources.get(res) / resources.get(res)) * points;
-                            result.put(res, resourcePoints);
-                        }
-                        return Collections.min(result.values());
-                    };
-            
-            case "resource" ->
-                    (Player player) -> {
-                        Map<Resource, Integer> playerResources = player.getResources();
-                        return playerResources.get(resource) * points;
-                    };
-
-            default -> throw new InvalidTypeException(type + "is invalid");
-        };
-    }
-
-    /**
      * Rotate a set of coordinates with value between -1 and 1 (inclusive)
+     *
      * @param coordinates an offset of coordinates in range (-1, 1) inclusive
      * @return a new set of coordinates, rotated clockwise 45deg
      */
@@ -179,9 +43,140 @@ public class FunctionBuilder {
         if (y != 0) {
             y = (int) (signum(y));
         }
-        result = new Coordinates(
-                x, y
-        );
+        result = new Coordinates(x, y);
         return result;
+    }
+
+    /**
+     * Set the resources and their amount to use to calculate the score when paired
+     * with the type "resources"
+     *
+     * @param resources the resource the function will use to calculate the score
+     * @return this object
+     * @see #setType(String)
+     * @see #setPoints(int)
+     * @see #build()
+     */
+    public FunctionBuilder setResources(Map<Resource, Integer> resources) {
+        this.resources = resources;
+        return this;
+    }
+
+    /**
+     * A multiplier of the points a certain type yields
+     *
+     * @param points the multiplier the function will use to calculate the score
+     * @return this object
+     */
+    public FunctionBuilder setPoints(int points) {
+        this.points = points;
+        return this;
+    }
+
+    /**
+     * A pattern that will yield points if it is matched on the game grid when the
+     * type is "pattern"
+     *
+     * @param pattern the pattern to use
+     * @return this object
+     * @see #setType(String)
+     * @see #setPoints(int)
+     * @see #build()
+     */
+    public FunctionBuilder setPattern(Resource[][] pattern) {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                if (pattern[y][x] != Resource.NONE) {
+                    this.pattern.put(rotate(new Coordinates(x - 1, y - 1)), pattern[y][x]);
+                }
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Set the type of the function, listed in {@link #getValidTypes()},
+     * to choose the strategy used to calculate the player score.
+     *
+     * @param type the type of the function calculation
+     * @return this object
+     * @throws InvalidTypeException when the type is not included in
+     *                              {@link #getValidTypes()}
+     */
+    public FunctionBuilder setType(String type) throws InvalidTypeException {
+        if (!Arrays.asList(validTypes).contains(type)) {
+            throw new InvalidTypeException(type + " is not a valid type");
+        }
+        this.type = type;
+        return this;
+    }
+
+    /**
+     * Set the resource to use to calculate the score when paired with the type
+     * "resource"
+     *
+     * @param resource the resource the function will use to calculate the score
+     * @return this object
+     * @see #setType(String)
+     * @see #setPoints(int)
+     * @see #build()
+     */
+    public FunctionBuilder setResource(Resource resource) {
+        this.resource = resource;
+        return this;
+    }
+
+    /**
+     * returns a function of the given type to calculate a player score, using the
+     * given type and score
+     *
+     * @return a function of the given type to calculate a player score
+     * @throws InvalidTypeException when the type is not included in
+     *                              {@link #getValidTypes()}
+     * @see #setType(String)
+     * @see Player
+     */
+    public Function<Player, Integer> build() throws InvalidTypeException {
+        return switch (type) {
+            case "none" -> (Player player) -> points;
+
+            case "pattern" -> (Player player) -> {
+                PlayerBoard board = player.getPlayerBoard();
+                int matches_found = new PatternMatcher().setPattern(pattern).setBoard(board).matches_found();
+                return matches_found * points;
+            };
+
+            case "cover" -> (Player player) -> {
+                PlayerBoard board = player.getPlayerBoard();
+                Coordinates lastCoordinates = board.getLastPlacedPosition();
+                int neighbor_count = 0;
+                for (Coordinates neighbor : lastCoordinates.getNeighbors()) {
+                    if (board.getCard(neighbor) != null || board.getCard(neighbor).getId() != 0) { // TODO test wether
+                                                                                                   // MockCards get
+                                                                                                   // counted
+                        neighbor_count++;
+                    }
+                }
+                return neighbor_count * points;
+            };
+
+            case "resources" -> (Player player) -> {
+                Map<Resource, Integer> playerResources = player.getResources();
+                int resourcePoints;
+                Map<Resource, Integer> result = new HashMap<>();
+                for (Resource res : resources.keySet()) {
+                    resourcePoints = (playerResources.get(res) / resources.get(res)) * points;
+                    result.put(res, resourcePoints);
+                }
+                return Collections.min(result.values());
+            };
+
+            case "resource" -> (Player player) -> {
+                Map<Resource, Integer> playerResources = player.getResources();
+                return playerResources.get(resource) * points;
+            };
+
+            default -> throw new InvalidTypeException(type + "is invalid");
+        };
     }
 }
