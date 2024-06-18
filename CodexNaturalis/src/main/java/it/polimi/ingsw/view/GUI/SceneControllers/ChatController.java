a9a286a2d5f19ccf4c1f7d41143e043e30d361a8
Samuele Franzese <106510615+SamueleFranzese02@users.noreply.github.com>
2024-06-18 23:11:00 +0200

    feat: added different chats for every player and general

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ChatController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/SceneControllers/ChatController.java
@@ -11,11 +11,13 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 import java.io.IOException;
 import java.net.URL;
@@ -39,6 +41,9 @@ public class ChatController implements Initializable, TabController {
     @FXML
     Button sendButton;
 
+    @FXML
+    ChoiceBox<String> playerChoiceChat;
+
     public void setClient(ClientInterface client, GameController gameController, SharedUpdate updater) {
         this.client = client;
         this.playerData = client.getPlayerData();
@@ -49,13 +54,14 @@ public class ChatController implements Initializable, TabController {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setChat();
+        setChoiceChat();
     }
 
     @FXML
     private void sendMessage(ActionEvent event) {
         if (!messageInput.getText().isEmpty()) {
             try {
-                client.postChat(messageInput.getText(), null);
+                client.postChat(messageInput.getText(), !playerChoiceChat.getValue().equals("General") ? playerChoiceChat.getValue() : null);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -73,24 +79,69 @@ public class ChatController implements Initializable, TabController {
             throw new RuntimeException(e);
         }
 
-        for (ChatMessage chatMessage: client.getChat()) {
-            HBox messageHBox = new HBox();
-            Text message = new Text(chatMessage.sender() + (chatMessage.receiver()!=null ? " to " + chatMessage.receiver() : "") + ": " + chatMessage.message());
-            message.setStyle("-fx-font-family: Trattatello;" +
-                    "-fx-font-size: 30;" +
-                    "-fx-text-fill: #432918;"
-            );
+        String selectedPlayer = playerChoiceChat.getValue();
 
-            if (chatMessage.sender().equals(client.getUsername())) {
-                messageHBox.setAlignment(Pos.CENTER_RIGHT);
-                messageHBox.setPadding(new Insets(0, 7, 0, 0));
+        for (ChatMessage chatMessage : client.getChat()) {
+            if (selectedPlayer.equals("General")) {
+                if (chatMessage.receiver() == null) {
+                    addMessageToChat(chatMessage);
+                }
             } else {
-                messageHBox.setAlignment(Pos.CENTER_LEFT);
-                messageHBox.setPadding(new Insets(0, 0, 0, 5));
+                if ((chatMessage.sender().equals(selectedPlayer) && chatMessage.receiver() != null && chatMessage.receiver().equals(client.getUsername())) || (chatMessage.receiver() != null && chatMessage.receiver().equals(selectedPlayer) && chatMessage.sender().equals(client.getUsername()))) {
+                    addMessageToChat(chatMessage);
+                }
             }
+        }
+    }
 
-            messageHBox.getChildren().add(message);
-            chatContainer.getChildren().add(messageHBox);
+    private void addMessageToChat(ChatMessage chatMessage) {
+        HBox messageHBox = new HBox();
+        VBox messageVBox = new VBox();
+
+        Text message = new Text(chatMessage.sender() + ":\n" + chatMessage.message());
+        message.setStyle("-fx-font-family: Trattatello;" +
+                "-fx-font-size: 30;" +
+                "-fx-text-fill: #432918;"
+        );
+
+        if (!chatMessage.sender().equals(client.getUsername())) {
+            message.setTextAlignment(TextAlignment.LEFT);
+        } else {
+            message.setTextAlignment(TextAlignment.RIGHT);
+        }
+        messageVBox.getChildren().add(message);
+
+
+        if (chatMessage.sender().equals(client.getUsername())) {
+            messageHBox.setAlignment(Pos.CENTER_RIGHT);
+            messageHBox.setPadding(new Insets(0, 7, 0, 0));
+        } else {
+            messageHBox.setAlignment(Pos.CENTER_LEFT);
+            messageHBox.setPadding(new Insets(0, 0, 0, 5));
+        }
+
+        messageHBox.getChildren().add(messageVBox);
+        chatContainer.getChildren().add(messageHBox);
+    }
+
+
+    @FXML
+    private void setChoiceChat() {
+        playerChoiceChat.getItems().clear();
+        playerChoiceChat.getItems().add("General");
+        playerChoiceChat.setValue("General");
+        playerChoiceChat.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> setChat());
+
+        try {
+            client.fetchPlayers();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (String player : client.getPlayers()) {
+            if (!player.equals(client.getUsername())) {
+                playerChoiceChat.getItems().add(player);
+            }
         }
     }
 
